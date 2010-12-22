0d825bae3e3a9cd4da00c792fb68bba1ba4d058f
lianas <lianas@0cdf8b0b-c6e5-4d59-8f63-774bc065f136>
2010-12-22 10:41:38 +0000

    Added simple delete example, minor bug fixes

--- tests/bl/lib/genotype/try_ome_tables.py
+++ tests/bl/lib/genotype/try_ome_tables.py
@@ -4,6 +4,8 @@ import omero_SharedResources_ice
 
 import vl.lib.utils as vl_utils
 
+import time
+
 
 def make_dummy_row():
     vid_col = omero.grid.StringColumn('vid','',len(vl_utils.make_vid()), None)
@@ -25,13 +27,13 @@ def make_dummy_row():
     return [vid_col, table_col, gene_build_col, bin_col, chrom_col,
             chrom_start_col, chrom_end_col, name_col]
 
-def make_table():
+def make_table(file_name):
     c = omero.client('localhost')
     s = c.createSession('root', 'omero')
     r = s.sharedResources()
     m = r.repositories()
     i = m.descriptions[0].id.val
-    t = r.newTable(i, 'SNPTable_v2.h5')
+    t = r.newTable(i, file_name)
     #Table columns
     vid_col = omero.grid.StringColumn('vid', 'Row VID', len(vl_utils.make_vid()), None)
     table_col = omero.grid.StringColumn('table', 'Referencing table from dbSNP', 15, None)
@@ -53,14 +55,30 @@ def read_table(file_name):
     s = c.createSession('root', 'omero')
     qs = s.getQueryService()
     ofile = qs.findByString('OriginalFile', 'name', file_name, None)
-    r = s.sharedResources()
-    t = r.openTable(ofile)
-    print t
-    print t.getNumberOfRows()
-    data = t.read([0,1,2,3], 0, t.getNumberOfRows())
-    print data
+    if ofile:
+        r = s.sharedResources()
+        t = r.openTable(ofile)
+        print t
+        print t.getNumberOfRows()
+        data = t.read([0,1,2,3], 0, t.getNumberOfRows())
+        #print data
+        c.closeSession()
+    else:
+        print 'No table found'
+
+# Right now, the only way to 'delete' an omeroTable is to remove the OriginalFile entry
+# from the database, the file containing the table in not delete from the file system
+def clear_table(file_name):
+    c = omero.client('localhost')
+    s = c.createSession('root', 'omero')
+    qs = s.getQueryService()
+    ofile = qs.findByString('OriginalFile', 'name', file_name, None)
+    us = s.getUpdateService()
+    us.deleteObject(ofile)
     c.closeSession()
 
-
-make_table()
-read_table('SNPTable_v2.h5')
+fname = 'DummyTable.h5'
+make_table(fname)
+read_table(fname)
+clear_table(fname)
+read_table(fname)
