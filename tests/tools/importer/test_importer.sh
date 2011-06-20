cecb89891281f84b659ea9cd03cee807eb52043c
zag <zag@0cdf8b0b-c6e5-4d59-8f63-774bc065f136>
2011-06-20 18:18:11 +0000

    vl/trunk added a full test_importer.sh

--- tests/tools/importer/test_importer.sh
+++ tests/tools/importer/test_importer.sh
@@ -8,7 +8,6 @@ ${IMPORTER} -i titer_plate.tsv titer_plate
 ${IMPORTER} -i plate_well.tsv plate_well
 ${IMPORTER} -i devices.tsv device
 ${IMPORTER} -i data_sample.tsv data_sample
-exit
 ${IMPORTER} -i data_object.tsv data_object
 ${IMPORTER} -i data_collection.tsv data_collection
 
@@ -16,7 +15,7 @@ ${IMPORTER} -i data_collection.tsv data_collection
 # use the following command to scratch and recreate the markers tables
 # THIS IS A VERY DANGEOURS THING TO DO.
 # If you are not sure, DO NOT DO IT!
-#../../../tools/create_snp_tables  -P romeo --do-it
+../../../tools/create_snp_tables  -P romeo --do-it
 #-----------------
 ${IMPORTER} -i AppliedBioSystem_TaqMan_MSstatus.tsv marker_definition -S BSTUDY --source CNR-IGMB --context TaqMan --release MSstatus
 ${IMPORTER} -i AppliedBioSystem_TaqMan_MSstatus_aligned.tsv marker_alignment -S BSTUDY --ref-genome hg28 --message 'alignment done using libbwa'
