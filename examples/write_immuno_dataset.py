f1e26a97f3e2f603d77c26f973ca5a7e3206c9c8
simleo <simleo@0cdf8b0b-c6e5-4d59-8f63-774bc065f136>
2011-11-24 14:35:00 +0000

    vl: examples/write_immuno_dataset.py: added input files for import

--- examples/write_immuno_dataset.py
+++ examples/write_immuno_dataset.py
@@ -1,11 +1,18 @@
 """ ...
 
 Read Illumina 'Final Report' text files and write snp calls for each
-well as an SSC (SampleSnpCall) file in the x-ssc-messages format.
+well as an SSC (SampleSnpCall) file in the x-ssc-messages
+format. Also, write tsv input files for DataSample and DataObject
+importers.
+
+**NOTE:** the program outputs the PlateWell *label* in the 'source'
+column: the mapping to the corresponding VID must be done by an
+external tool
 
 """
-import sys, os, argparse, logging
+import sys, os, argparse, hashlib, csv, logging
 from datetime import datetime
+from contextlib import nested
 
 from bl.core.io.illumina import GenomeStudioFinalReportReader as DataReader
 from bl.core.io.illumina import IllSNPReader
@@ -19,15 +26,30 @@ LOG_FORMAT = '%(asctime)s|%(levelname)-8s|%(message)s'
 LOG_DATEFMT = '%Y-%m-%d %H:%M:%S'
 LOG_LEVELS = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
 
-# FIXME should these be available as parameters?
+# FIXME should (some of) these be available as parameters?
 MSET_LABEL = "IMMUNO_BC_11419691_B"
 DEVICE_MAKER = "Illumina"
 DEVICE_MODEL = "GenomeStudio"
-#DEVICE_TYPE = "SoftwareProgram"
+DEVICE_TYPE = "SoftwareProgram"
+DATA_SAMPLE_TYPE = "GenotypeDataSample"
+DS_FN = "import_data_sample.tsv"
+DO_FN = "import_data_object.tsv"
 
 PAYLOAD_MSG_TYPE = 'core.gt.messages.SampleSnpCall'
 
 PAD_SIZE = 12
+
+
+# copied from import_taqman_results.py
+def compute_sha1(fname):
+  BUFSIZE = 10000000
+  sha1 = hashlib.sha1()
+  with open(fname) as fi:
+    s = fi.read(BUFSIZE)
+    while s:
+      sha1.update(s)
+      s = fi.read(BUFSIZE)
+  return sha1.hexdigest()
 
 
 def adjust_immuno_sample_id(old_sample_id, plate_barcode, pad_size=PAD_SIZE):
@@ -48,6 +70,9 @@ class Writer(Core):
     'BB': (0., 0., 1.),
     '--': (.5, 0., .5),
     }
+  DS_HEADER = ["label", "source", "device", "device_type", "data_sample_type",
+               "markers_set"]
+  DO_HEADER = ["path", "data_sample_label", "mimetype", "size", "sha1"]
   
   def critical(self, msg):
     self.logger.critical(msg)
@@ -60,6 +85,7 @@ class Writer(Core):
     self.logger.info("loading markers for marker set %s" % mset_label)
     ms.load_markers()
     self.logger.info("%s has %d markers" % (mset_label, len(ms.markers)))
+    self.markers_set_id = ms.id
     self.marker_ids = dict((m.label, m.id) for m in ms.markers)
 
   def get_marker_name_to_label(self, ill_annot_file):
@@ -84,19 +110,32 @@ class Writer(Core):
       self.critical("no container in kb is labeled as %s" % plate_label)
     return plate.barcode
 
-  def write_dataset_files(self, in_fnames, prefix):
-    for fn in in_fnames:
-      self.logger.info("processing %s" % fn)
-      plate_barcode = self.get_plate_barcode(fn)
-      with open(fn) as f:
-        data_reader = DataReader(f)
-        release = data_reader.header.get("GSGT Version", "UNKNOWN_RELEASE")
-        label = "%s.%s.%s" % (DEVICE_MAKER, DEVICE_MODEL, release)
-        device = self.get_device(label, DEVICE_MAKER, DEVICE_MODEL, release)
-        n_blocks = data_reader.header.get('Num Samples', '?')
-        for i, block in enumerate(data_reader.get_sample_iterator()):
-          self.logger.info("processing block %d/%s " % (i+1, n_blocks))
-          self.write(block, device.id, plate_barcode, prefix)
+  def write_output_files(self, in_fnames, prefix):
+    with nested(open(DS_FN, "w"), open(DO_FN, "w")) as (ds_f, do_f):
+      ds_w = csv.writer(ds_f, delimiter="\t", lineterminator=os.linesep)
+      do_w = csv.writer(do_f, delimiter="\t", lineterminator=os.linesep)
+      ds_w.writerow(self.DS_HEADER)
+      do_w.writerow(self.DO_HEADER)
+      for fn in in_fnames:
+        self.logger.info("processing %s" % fn)
+        plate_barcode = self.get_plate_barcode(fn)
+        with open(fn) as f:
+          data_reader = DataReader(f)
+          release = data_reader.header.get("GSGT Version", "UNKNOWN_RELEASE")
+          label = "%s.%s.%s" % (DEVICE_MAKER, DEVICE_MODEL, release)
+          device = self.get_device(label, DEVICE_MAKER, DEVICE_MODEL, release)
+          n_blocks = data_reader.header.get('Num Samples', '?')
+          for i, block in enumerate(data_reader.get_sample_iterator()):
+            self.logger.info("processing block %d/%s " % (i+1, n_blocks))
+            sample_id, fn = self.write(block, device.id, plate_barcode, prefix)
+            label = os.path.basename(fn)
+            self.logger.info("computing checksum for %s" % fn)
+            path = os.path.abspath(fn)
+            sha1 = compute_sha1(fn)
+            size = str(os.stat(fn).st_size)
+            ds_w.writerow([label, sample_id, device.id, DEVICE_TYPE,
+                           DATA_SAMPLE_TYPE, self.markers_set_id])
+            do_w.writerow([path, sample_id, 'x-ssc-messages', size, sha1])
 
   def write(self, data_block, device_id, plate_barcode, prefix):
     sample_id = adjust_immuno_sample_id(data_block.sample_id, plate_barcode)
@@ -124,6 +163,7 @@ class Writer(Core):
         'weight_BB': w_bb,
         })
     stream.close()
+    return sample_id, out_fn
 
 
 def make_parser():
@@ -161,7 +201,7 @@ def main(argv):
   writer.get_marker_ids(MSET_LABEL)
   writer.get_marker_name_to_label(args.annot_file)
   writer.get_marker_name_to_id()
-  writer.write_dataset_files(args.ifiles, args.prefix)
+  writer.write_output_files(args.ifiles, args.prefix)
 
 
 if __name__ == "__main__":
