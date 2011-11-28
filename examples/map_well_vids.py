31e5141255c74639588f522869f42d1b18d063df
simleo <simleo@0cdf8b0b-c6e5-4d59-8f63-774bc065f136>
2011-11-28 10:07:48 +0000

    vl: examples/map_well_vids.py: minor fixes

--- examples/map_well_vids.py
+++ examples/map_well_vids.py
@@ -17,6 +17,40 @@ LOG_LEVELS = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
 STUDY = "IMMUNOCHIP"
 
 
+class VidMapper(object):
+
+  def __init__(self, host, user, passwd, logger):
+    self.logger = logger
+    self.kb = KB(driver="omero")(host, user, passwd)
+    plates = self.kb.get_objects(self.kb.TiterPlate)
+    self.logger.info("fetched %d plates" % len(plates))
+    self.plate_map = {}
+    self.enroll_map = {}
+    for p in plates:
+      self.plate_map[p.omero_id] = p.barcode
+    s = self.kb.get_study(STUDY)
+    enrolls = self.kb.get_enrolled(s)
+    self.logger.info("fetched %d enrollments" % len(enrolls))
+    for e in enrolls:
+      wells = [w for w in self.kb.get_vessels_by_individual(e.individual,
+                                                            "PlateWell")]
+      self.enroll_map[e.studyCode] = wells
+
+  def map_vid(self, r):
+    en_code = r["source"]
+    pl_barcode = en_code.split("|")[1]
+    wells = self.enroll_map[en_code]
+    self.logger.info("found %d wells for %s" % (len(wells), en_code))
+    for w in wells:
+      if self.plate_map[w.container.omero_id] == pl_barcode:
+        r["source"] = w.id
+        break
+    else:
+      msg = "no well for plate %s" % pl_barcode
+      self.logger.warn(msg)
+      raise ValueError(msg)
+
+
 def make_parser():
   parser = argparse.ArgumentParser(description="map immuno vids")
   parser.add_argument('-i', '--input-file', metavar='FILE', required=True,
@@ -57,40 +91,6 @@ def main(argv):
         continue
       else:
         writer.writerow(r)
-
-
-class VidMapper(object):
-
-  def __init__(self, host, user, passwd, logger):
-    self.logger = logger
-    self.kb = KB(driver="omero")(host, user, passwd)
-    plates = self.kb.get_objects(self.kb.TiterPlate)
-    self.logger.info("fetched %d plates" % len(plates))
-    self.plate_map = {}
-    self.enroll_map = {}
-    for p in plates:
-      self.plate_map[p.ome_obj.id._val] = p.barcode
-    s = self.kb.get_study(STUDY)
-    enrolls = self.kb.get_enrolled(s)
-    self.logger.info("fetched %d enrollments" % len(enrolls))
-    for e in enrolls:
-      wells = [w for w in self.kb.get_vessels_by_individual(e.individual,
-                                                       "PlateWell")]
-      self.enroll_map[e.studyCode] = wells
-
-  def map_vid(self, r):
-    en_code = r["source"]
-    pl_barcode = en_code.split("|")[1]
-    wells = self.enroll_map[en_code]
-    self.logger.info("found %d wells for %s" % (len(wells), en_code))
-    for w in wells:
-      if self.plate_map[w.container.ome_obj.id._val] == pl_barcode:
-        r["source"] = w.id
-        break
-    else:
-      msg = "no well for plate %s" % pl_barcode
-      self.logger.warn(msg)
-      raise ValueError(msg)
 
 
 if __name__ == "__main__":
