92e1ec6ff74efcd5424c0acbd1e4ab6a72aa3ea7
simleo <simleo@0cdf8b0b-c6e5-4d59-8f63-774bc065f136>
2011-12-16 17:18:53 +0000

    vl: tools/gdoize_ms and do importer cleaned up

--- bl/vl/app/importer/data_object.py
+++ bl/vl/app/importer/data_object.py
@@ -1,31 +1,31 @@
 """
-Import of Data Objects
+Import of data objects
 ======================
 
-Will read in a tsv file with the following columns::
+Will read in a tsv file with at least the following columns::
 
-   study path data_sample mimetype size sha1
-   TEST01 file:/share/fs/v039303.cel V2902 x-vl/affymetrix-cel 39090 SHA1SHA1
-   ....
+   path data_sample size sha1
+   /share/fs/v039303.cel V02902 39090 SHA1SHA1
+   ...
 
-and will instantiate for each a DataObject that represent, inside
-omero/VL, a physical instance of a given DataSample.
+and will instantiate, for each row, a DataObject that represents a
+physical instance of a given DataSample.
 
-Records that point to an unknown (data_sample) will be noisily
+Records that point to an unknown data sample vid will be noisily
 ignored. The same will happen to records that have the same path of a
-previously seen data_object
+previously seen data object.
 
 .. code-block:: bash
 
   bash> cat data_object.tsv
   study path  data_sample_label mimetype  size  sha1
-  BSTUDY  file:/share/fs/v000.cel foobar-00 x-vl/affymetrix-cel 8989  SHA1SHA1
-  BSTUDY  file:/share/fs/v001.cel foobar-01 x-vl/affymetrix-cel 8989  SHA1SHA1
-  BSTUDY  file:/share/fs/v002.cel foobar-02 x-vl/affymetrix-cel 8989  SHA1SHA1
-  BSTUDY  file:/share/fs/v003.cel foobar-03 x-vl/affymetrix-cel 8989  SHA1SHA1
-  BSTUDY  file:/share/fs/v004.cel foobar-04 x-vl/affymetrix-cel 8989  SHA1SHA1
-  BSTUDY  file:/share/fs/v005.cel foobar-05 x-vl/affymetrix-cel 8989  SHA1SHA1
-  BSTUDY  file:/share/fs/v051.cel foobar-05 x-vl/affymetrix-cel 8989  SHA1SHA1
+  BSTUDY  /share/fs/v000.cel foobar-00 x-vl/affymetrix-cel 8989  SHA1SHA1
+  BSTUDY  /share/fs/v001.cel foobar-01 x-vl/affymetrix-cel 8989  SHA1SHA1
+  BSTUDY  /share/fs/v002.cel foobar-02 x-vl/affymetrix-cel 8989  SHA1SHA1
+  BSTUDY  /share/fs/v003.cel foobar-03 x-vl/affymetrix-cel 8989  SHA1SHA1
+  BSTUDY  /share/fs/v004.cel foobar-04 x-vl/affymetrix-cel 8989  SHA1SHA1
+  BSTUDY  /share/fs/v005.cel foobar-05 x-vl/affymetrix-cel 8989  SHA1SHA1
+  BSTUDY  /share/fs/v051.cel foobar-05 x-vl/affymetrix-cel 8989  SHA1SHA1
   bash> ${KB_QUERY} -o data_object_mapped.tsv \
                map_vid -i data_object.tsv \
                    --column data_sample_label,data_sample \
@@ -33,15 +33,11 @@ previously seen data_object
                    --study BSTUDY
   bash> ${IMPORTER} -i data_object_mapped.tsv -o data_object_mapping.tsv \
                data_object
-
 """
-from core import Core, BadRecord
-
-import csv, json, time
-
-import itertools as it
+import csv
 
 from bl.vl.kb.mimetypes import DATA_OBJECT_FILES as SUPPORTED_MIME_TYPES
+from core import Core
 
 
 class Recorder(Core):
@@ -64,18 +60,13 @@ class Recorder(Core):
       while len(records[offset:]) > 0:
         yield records[offset:offset+batch_size]
         offset += batch_size
-
     if not records:
       self.logger.warn('no records')
       return
-
     study = self.find_study(records)
-
     self.preload_data_samples()
     self.preload_data_objects()
-
     records = self.do_consistency_checks(records)
-
     for i, c in enumerate(records_by_chunk(self.batch_size, records)):
       self.logger.info('start processing chunk %d' % i)
       self.process_chunk(c)
@@ -95,35 +86,28 @@ class Recorder(Core):
 
   def do_consistency_checks(self, records):
     self.logger.info('start consistency checks')
-    #--
     good_records = []
     mandatory_fields = ['path', 'mimetype', 'size', 'sha1']
     for i, r in enumerate(records):
       reject = 'Rejecting import of record %d: ' % i
-
       if self.missing_fields(mandatory_fields, r):
         continue
-
       if r['path'] in self.preloaded_data_objects:
         f = reject + 'there is a pre-existing data_object with path %s.'
         self.logger.warn(f % r['path'])
         continue
-
       if not r['data_sample'] in self.preloaded_data_samples:
         f = reject + 'there is no known data_sample with id %s.'
         self.logger.error(f % r['data_sample'])
         continue
-
       if r['mimetype'] not in SUPPORTED_MIME_TYPES:
         f = reject + 'unknown mimetype %s.'
         self.logger.error(f % r['mimetype'])
         continue
-
       if not r['size'].isdigit():
         f = reject + 'bad size value %s.'
         self.logger.error(f % r['size'])
         continue
-
       good_records.append(r)
     self.logger.info('done consistency checks')
     return good_records
@@ -138,55 +122,52 @@ class Recorder(Core):
               'sample' : sample,
               'sha1' : r['sha1']}
       data_objects.append(self.kb.factory.create(self.kb.DataObject, conf))
-    #--
     self.kb.save_array(data_objects)
     for do in data_objects:
       self.logger.info('saved %s[%s,%s] as attached to %s' %
                        (do.path, do.mimetype, do.size, do.sample.id))
 
+
 def canonize_records(args, records):
   fields = ['study', 'mimetype']
   for f in fields:
-    if hasattr(args, f) and getattr(args,f) is not None:
+    v = getattr(args, f, None)
+    if v is not None:
       for r in records:
-        r[f] = getattr(args, f)
+        r[f] = v
+
 
 def make_parser_data_object(parser):
   parser.add_argument('--study', type=str,
                       help="""default study used as context
                       for the import action.  It will
-                      over-ride the study column value, if any.""")
+                      override the study column value, if any""")
   parser.add_argument('--mimetype', type=str,
                       choices=SUPPORTED_MIME_TYPES,
                       help="""default mimetype.  It will
-                      over-ride the mimetype column value, if any.""")
+                      override the mimetype column value, if any""")
+
 
 def import_data_object_implementation(logger, args):
-
   action_setup_conf = Recorder.find_action_setup_conf(args)
-
   recorder = Recorder(args.study,
                       host=args.host, user=args.user, passwd=args.passwd,
                       operator=args.operator,
                       action_setup_conf=action_setup_conf,
                       logger=logger)
-
-  #--
   f = csv.DictReader(args.ifile, delimiter='\t')
   logger.info('start processing file %s' % args.ifile.name)
   records = [r for r in f]
-
   canonize_records(args, records)
-
   recorder.record(records)
-
   logger.info('done processing file %s' % args.ifile.name)
-  #--
+
 
 help_doc = """
-import new data object definitions into a virgil system and attach
+import new data object definitions into OMERO.biobank and attach
 them to previously registered data samples.
 """
+
 
 def do_register(registration_list):
   registration_list.append(('data_object', help_doc,
