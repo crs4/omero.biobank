b30f6b8aefbdc5c516103035edae53985b9a2e5a
simleo <simleo@0cdf8b0b-c6e5-4d59-8f63-774bc065f136>
2011-12-21 16:38:15 +0000

    vl: write_immuno_dataset made kb-agnostic except for sample id adjustment hack

--- bl/vl/app/importer/device.py
+++ bl/vl/app/importer/device.py
@@ -1,65 +1,28 @@
 """
-Import of Device(s)
-===================
+Import of devices
+=================
 
-Will read in a tsv file with the following columns::
+Will read in a tsv file with the following columns (multiple spaces
+represent tabs)::
 
-  study  device_type    label   barcode maker model release location
-  BSTUDY Scanner pula01  8989898 Affymetrix  GeneChip Scanner 3000 7G  Pula bld. 5
-  BSTUDY Chip    chip001 8329482 Affymetrix  Genome-Wide Human SNP Array 6.0  None
+  device_type  label   maker       model                        release
+  Scanner      pula01  Affymetrix  GeneChip Scanner 3000        7G
+  Chip         chip01  Affymetrix  Genome-Wide Human SNP Array  6.0
 
-All devices have a type, a label, an optional barcode, a maker, a
-model and a release, and, possibly a physical location. In the example
-above, in the first line we have defined a scanner, which is
-physically in the lab in Pula, building 5.  The following line defines
-a chip. A special case
-
-.. code-block:: bash
-
-  bash> cat devices.tsv
-  device_type label barcode maker model release location
-  Scanner pula01  8989898 Affymetrix  GeneChip Scanner 3000 7G  Pula Ed.5
-  Chip  chip001 8329482 Affymetrix  Genome-Wide Human SNP Array 6.0 None
-  Chip  chip002 8329483 Affymetrix  Genome-Wide Human SNP Array 6.0 None
-  Chip  chip003 8329484 Affymetrix  Genome-Wide Human SNP Array 6.0 None
-  Chip  chip004 8329485 Affymetrix  Genome-Wide Human SNP Array 6.0 None
-  Chip  chip005 8329486 Affymetrix  Genome-Wide Human SNP Array 6.0 None
-  Chip  chip006 8329487 Affymetrix  Genome-Wide Human SNP Array 6.0 None
-  bash> ${IMPORTER} -i devices.tsv -o devices_mapping.tsv device --study BSTUDY
-  bash> cat devices_mapping.tsv
-  study label type  vid
-  BSTUDY  pula01  Scanner V0BAF6C3F471284CBF9A867A62B26E688A
-  BSTUDY  chip001 Chip  V03C9FB0C268594D7BBBB359BBF09163E4
-  BSTUDY  chip002 Chip  V03C658EE6C1E8494FB239E791EA0BA061
-  BSTUDY  chip003 Chip  V0E37789C133784DB9B9FF6EA81A3B1024
-  BSTUDY  chip004 Chip  V0ABA7C0D1D90F46F7B36F1FB8ED626AEF
-  BSTUDY  chip005 Chip  V02CA380A20926473386A7A15A5270B939
-  BSTUDY  chip006 Chip  V065049E68E56946E09CE51E448756F01F
-
+All devices have a type, a label, a maker, a model and a release;
+optional fields are 'barcode' and 'location'.
 """
 
-from core import Core, BadRecord
-from version import version
+import csv, os
+from core import Core
 
-import itertools as it
-import csv, json
-import time, sys
 
 class Recorder(Core):
-  """
-  An utility class that handles the actual recording of Device(s)
-  into VL, including Device(s) generation as needed.
-  """
-  def __init__(self,
-               study_label,
+  
+  def __init__(self, study_label,
                host=None, user=None, passwd=None, keep_tokens=1,
-               batch_size
-               =1000, operator='Alfred E. Neumann',
-               action_setup_conf=None,
-               logger=None):
-    """
-    FIXME
-    """
+               batch_size=1000, operator='Alfred E. Neumann',
+               action_setup_conf=None, logger=None):
     super(Recorder, self).__init__(host, user, passwd, keep_tokens=keep_tokens,
                                    study_label=study_label, logger=logger)
     self.batch_size = batch_size
@@ -75,17 +38,13 @@ class Recorder(Core):
       while len(records[offset:]) > 0:
         yield records[offset:offset+batch_size]
         offset += batch_size
-    #--
     if not records:
       self.logger.warn('no records')
       return
-    #--
     study  = self.find_study(records)
     self.preload_devices()
     self.preload_markers_sets()
-
     records = self.do_consistency_checks(records)
-
     for i, c in enumerate(records_by_chunk(self.batch_size, records)):
       self.logger.info('start processing chunk %d' % i)
       self.process_chunk(c, otsv, study)
@@ -108,17 +67,14 @@ class Recorder(Core):
   def do_consistency_checks(self, records):
     self.logger.info('start consistency checks')
     k_map = {}
-    #--
     good_records = []
     mandatory_fields = ['label', 'maker', 'model', 'release']
     for i, r in enumerate(records):
       reject = 'Rejecting import of record %d: ' % i
-
       if self.missing_fields(mandatory_fields, r):
         f = reject + 'missing mandatory field.'
         self.logger.error(f)
         continue
-
       if r['label'] in k_map:
         f = (reject +
              'there is a pre-existing device with label %s. (in this batch).')
@@ -128,12 +84,10 @@ class Recorder(Core):
         f = reject + 'there is a pre-existing device with label %s.'
         self.logger.warn(f % r['label'])
         continue
-
       if r['barcode'] and r['barcode'] in self.known_barcodes:
         m = reject + 'there is a pre-existing object with barcode %s.'
         self.logger.warn(m % r['barcode'])
         continue
-
       if 'device_type' not in r:
         f = reject + 'missing device_type for record with label %s.'
         self.logger.error(f % r['label'])
@@ -154,11 +108,9 @@ class Recorder(Core):
                'device label %s is a GenotypingProgram, unknown markers_set')
           self.logger.error(f % r['label'])
           continue
-
       k_map['label'] = r
       good_records.append(r)
     self.logger.info('done consistency checks')
-    #--
     return good_records
 
   def process_chunk(self, chunk, otsv, study):
@@ -176,72 +128,73 @@ class Recorder(Core):
         conf['snpMarkersSet'] = self.preloaded_markers_sets[r['markers_set']]
       devices.append(self.kb.factory.create(dklass, conf))
     self.kb.save_array(devices)
-    #--
     for d in devices:
-      otsv.writerow({'study' : study.label,
-                     'label' : d.label,
-                     'type'  : d.get_ome_table(),
-                     'vid'   : d.id })
+      otsv.writerow({
+        'study': study.label,
+        'label': d.label,
+        'type': d.get_ome_table(),
+        'vid': d.id
+        })
+
 
 def canonize_records(args, records):
   fields = ['study', 'maker', 'model', 'release', 'device_type']
   for f in fields:
-    if hasattr(args, f) and getattr(args,f) is not None:
+    v = getattr(args, f, None)
+    if v is not None:
       for r in records:
-        r[f] = getattr(args, f)
-  # specific fixes
+        r[f] = v
   for k in ['location', 'barcode', 'markers_set']:
     for r in records:
       if not (k in r and (r[k] and r[k].upper() != 'NONE')):
         r[k] = None
 
+
 help_doc = """
-import new Device definitions into a virgil system.
+import new Device definitions into the biobank.
 """
 
+
 def make_parser_device(parser):
-  parser.add_argument('--study', type=str,
-                      help="""default conxtest study label.
-                      It will over-ride the study column value""")
-  parser.add_argument('--device-type', type=str,
-                      choices=['Chip', 'Scanner',
-                               'SoftwareProgram', 'GenotypingProgram'],
-                      help="""default device type.  It will
-                      over-ride the container_type column value, if any.
-                      """)
-  parser.add_argument('--maker', type=str,
-                      help="""the device maker,
-                      it will override the corresponding column""")
-  parser.add_argument('--model', type=str,
-                      help="""the device model,
-                      it will override the corresponding column""")
-  parser.add_argument('--release', type=str,
-                      help="""the device release,
-                      it will override the corresponding column""")
+  parser.add_argument(
+    '--study', type=str, metavar="STR",
+    help="Study label. Overrides the study column value, if any"
+    )
+  parser.add_argument(
+    '--device-type', type=str, metavar="STR",
+    choices=['Chip', 'Scanner', 'SoftwareProgram', 'GenotypingProgram'],
+    help="Device type. Overrides the device_type column value, if any"
+    )
+  parser.add_argument(
+    '--maker', type=str, metavar="STR",
+    help="Device maker. Overrides the maker column value"
+    )
+  parser.add_argument(
+    '--model', type=str, metavar="STR",
+    help="Device model. Overrides the model column value"
+    )
+  parser.add_argument(
+    '--release', type=str, metavar="STR",
+    help="Device release. Overrides the release column value"
+    )
 
 
 def import_device_implementation(logger, args):
-
   action_setup_conf = Recorder.find_action_setup_conf(args)
-
   recorder = Recorder(args.study,
                       host=args.host, user=args.user, passwd=args.passwd,
                       operator=args.operator,
                       action_setup_conf=action_setup_conf,
-                      keep_tokens=args.keep_tokens,
-                      logger=logger)
+                      keep_tokens=args.keep_tokens, logger=logger)
   f = csv.DictReader(args.ifile, delimiter='\t')
   logger.info('start processing file %s' % args.ifile.name)
   records = [r for r in f]
-
   canonize_records(args, records)
-
-  o = csv.DictWriter(args.ofile,
+  o = csv.DictWriter(args.ofile, delimiter='\t',
                      fieldnames=['study', 'label', 'type', 'vid'],
-                     delimiter='\t')
+                     lineterminator=os.linesep)
   o.writeheader()
   recorder.record(records, o)
-
   logger.info('done processing file %s' % args.ifile.name)
 
 
@@ -249,5 +202,3 @@ def do_register(registration_list):
   registration_list.append(('device', help_doc,
                             make_parser_device,
                             import_device_implementation))
-
-
--- examples/write_immuno_dataset.py
+++ examples/write_immuno_dataset.py
@@ -5,18 +5,18 @@ sample as an SSC (SampleSnpCall) file in the mimetypes.SSC_FILE
 format. Also, write tsv input files for DataSample and DataObject
 importers.
 
-The program outputs the sample *label* in the 'source'
-column: the mapping to the corresponding PlateWell VID must be done by
-an external tool
+The program outputs the sample *label* in the 'source' column: the
+mapping to the corresponding PlateWell VID must be done by an external
+tool. The same holds for the 'device' and the 'markers_set'
+columns. Note that objects corresponding to these labels need *not*
+exist at the time this program is run.
 
-## MSET_LABEL = "IMMUNO_BC_11419691_B"
-## DEVICE_MAKER = "Illumina"
-## DEVICE_MODEL = "GenomeStudio"
-## DEVICE_TYPE = "SoftwareProgram"
+Here is an example of tsv input for the import device tool (multiple
+spaces represent tabs)::
 
-## release = data_reader.header.get("GSGT Version", "UNKNOWN_RELEASE")
-## label = "%s.%s.%s" % (DEVICE_MAKER, DEVICE_MODEL, release)
-## device = self.get_device(label, DEVICE_MAKER, DEVICE_MODEL, release)
+  device_type      label                        maker     model         release
+  SoftwareProgram  Illumina-GenomeStudio-1.8.4  Illumina  GenomeStudio  1.8.4
+
 """
 import sys, os, argparse, csv, logging
 from contextlib import nested
@@ -26,7 +26,6 @@ from bl.core.io.illumina import IllSNPReader
 from bl.core.io import MessageStreamWriter
 import bl.core.gt.messages.SnpCall as SnpCall
 from bl.vl.kb import KBError, mimetypes, KnowledgeBase as KB
-from bl.vl.app.importer.core import Core  # move this script to importer app?
 from bl.vl.utils import compute_sha1
 
 
@@ -37,6 +36,7 @@ LOG_LEVELS = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
 DS_FN = "import_data_sample.tsv"
 DO_FN = "import_data_object.tsv"
 
+DEVICE_TYPE = "SoftwareProgram"
 DATA_SAMPLE_TYPE = "GenotypeDataSample"
 PAYLOAD_MSG_TYPE = 'core.gt.messages.SampleSnpCall'
 
@@ -46,7 +46,6 @@ WEIGHTS = {
   'BB': (0., 0., 1.),
   '--': (1/3., 1/3., 1/3.),
   }
-DS_
 
 PAD_SIZE = 12
 
@@ -60,7 +59,7 @@ class Writer(csv.DictWriter):
 class DataSampleWriter(Writer):
   def __init__(self, f):
     Writer.__init__(self, f, ["label", "source", "device", "device_type",
-                              "data_sample_type", "status"])
+                              "data_sample_type", "markers_set", "status"])
 
 
 class DataObjectWriter(Writer):
@@ -75,38 +74,34 @@ def get_snp_name_to_label(ill_annot_file):
     return dict((r["Name"], r["IlmnID"]) for r in reader)
 
 
+#--- this is a CRS4-specific hack to fix ambiguous immunochip labels -------
+#--- this script should be kb-agnostic and would be so but for this hack ---
 def adjust_immuno_sample_id(old_sample_id, plate_barcode, pad_size=PAD_SIZE):
-  """
-  This is a CRS4-specific hack to fix ambiguous immunochip labels.
-  """
   padded_id = old_sample_id.rjust(pad_size, '0')
   return "%s|%s" % (padded_id, plate_barcode)
-
-
-def critical(logger, msg):
-  logger.critical(msg)
-  raise KBError(msg)
-
 
 def get_plate_barcode(kb, fn):
   plate_fields = os.path.splitext(os.path.basename(fn))[0].split("_")[1:-1]
   plate_label = "_".join(plate_fields+["imm"])
   plate = kb.get_container(plate_label)
   if plate is None:
-    critical("no container in kb is labeled as %s" % plate_label)
+    msg = "no container in kb is labeled as %s" % plate_label
+    logger.critical(msg)
+    raise KBError(msg)
   return plate.barcode
+#---------------------------------------------------------------------------
 
 
-def write_block(self, data_block, plate_barcode, out_dir, snp_name_to_label):
+def write_block(data_block, plate_barcode, out_dir, snp_name_to_label, header):
   sample_id = adjust_immuno_sample_id(data_block.sample_id, plate_barcode)
   out_fn = os.path.join(out_dir, '%s.ssc' % sample_id)
-  header = {'sample_id': sample_id}
+  header['sample_id'] = sample_id
   stream = MessageStreamWriter(out_fn, PAYLOAD_MSG_TYPE, header)
   for k in data_block.snp_names():
     snp = data_block.snp[k]
-    snp_id = name_to_label[snp['SNP Name']]
+    snp_id = snp_name_to_label[snp['SNP Name']]
     call = '%s%s' % (snp['Allele1 - AB'], snp['Allele2 - AB'])
-    w_aa, w_ab, w_bb = self.WEIGHTS[call]
+    w_aa, w_ab, w_bb = WEIGHTS[call]
     # GC Score = 0.0 ==> call = '--'
     stream.write({
       'sample_id': sample_id,
@@ -140,7 +135,9 @@ def make_parser():
                       default='root')
   parser.add_argument('-P', '--passwd', type=str, help='omero password',
                       required=True)
-  parser.add_argument('--device-vid', type=str, help='device vid',
+  parser.add_argument('--device-label', type=str, help='device label',
+                      required=True)
+  parser.add_argument('--marker-set-label', type=str, help='marker set label',
                       required=True)
   parser.add_argument('--ds-fn', metavar='DS_FILE', default=DS_FN,
                       help='output path for import data sample tsv file')
@@ -161,44 +158,45 @@ def main(argv):
   logger = logging.getLogger()
 
   kb = KB(driver="omero")(args.host, args.user, args.passwd)
-  try:
-    device = kb.get_by_vid(kb.Device, args.device_vid)
-  except ValueError, e:
-    sys.exit(e)
-
   snp_name_to_label = get_snp_name_to_label(args.annot_file)
 
   with nested(open(args.ds_fn, "w"), open(args.do_fn, "w")) as (ds_f, do_f):
-  ds_w = DataSampleWriter(ds_f)
-  do_w = DataObjectWriter(do_f)
-  for w in ds_w, do_w:
-    w.writeheader()
-  for fn in args.ifiles:
-    logger.info("processing %s" % fn)
-    plate_barcode = get_plate_barcode(kb, fn)
-    with open(fn) as f:
-      data_reader = DataReader(f)
-      n_blocks = data_reader.header.get('Num Samples', '?')
-      for i, block in enumerate(data_reader.get_sample_iterator()):
-        logger.info("processing block %d/%s " % (i+1, n_blocks))
-        sample_id, out_fn = write_block(block, plate_barcode, out_dir,
-                                        snp_name_to_label)
-        label = os.path.basename(out_fn)
-        out_path = os.path.abspath(out_fn)
-        logger.info("computing checksum for %s" % label)
-        sha1 = compute_sha1(out_path)
-        size = str(os.stat(out_path).st_size)
-        ds_w.writerow({
-          "label": label,
-          "source": sample_id,
-          "device": device.id,
-          "device_type": device.__class__.__name__,
-          "data_sample_type": DATA_SAMPLE_TYPE,
-          "status": "USABLE"
-          })
-        do_w.writerow([path, label, mimetypes.SSC_FILE, size, sha1])
-        for outf in ds_f, do_f:
-          outf.flush(); os.fsync(outf.fileno())
+    ds_w = DataSampleWriter(ds_f)
+    do_w = DataObjectWriter(do_f)
+    for w in ds_w, do_w:
+      w.writeheader()
+    for fn in args.ifiles:
+      logger.info("processing %s" % fn)
+      plate_barcode = get_plate_barcode(kb, fn)
+      with open(fn) as f:
+        data_reader = DataReader(f)
+        header = data_reader.header
+        header['markers_set'] = args.marker_set_label
+        n_blocks = header.get('Num Samples', '?')
+        for i, block in enumerate(data_reader.get_sample_iterator()):
+          logger.info("processing block %d/%s " % (i+1, n_blocks))
+          sample_id, out_fn = write_block(block, plate_barcode, args.out_dir,
+                                          snp_name_to_label, header)
+          label = os.path.basename(out_fn)
+          out_path = os.path.abspath(out_fn)
+          ds_w.writerow({
+            "label": label,
+            "source": sample_id,
+            "device": args.device_label,
+            "device_type": DEVICE_TYPE,
+            "data_sample_type": DATA_SAMPLE_TYPE,
+            "markers_set": args.marker_set_label,
+            "status": "USABLE",
+            })
+          do_w.writerow({
+            "path": out_path,
+            "data_sample_label": label,
+            "mimetype": mimetypes.SSC_FILE,
+            "size": str(os.stat(out_path).st_size),
+            "sha1": compute_sha1(out_path),
+            })
+          for outf in ds_f, do_f:
+            outf.flush(); os.fsync(outf.fileno())
 
 
 if __name__ == "__main__":
