12552f76fb8300cbafea870dc8544a51f75bbb13
simleo <simleo@0cdf8b0b-c6e5-4d59-8f63-774bc065f136>
2012-01-30 16:15:33 +0000

    vl: added np_ext to utils

---
+++ bl/vl/utils/np_ext.py
@@ -0,0 +1,25 @@
+"""
+NumPy extensions.
+"""
+
+import numpy as np
+
+
+def index_intersect(a1, a2):
+  """
+  Find indexes of elements common to arrays a1 and a2.
+
+  a1 and a2 must have the same dtype and contain no duplicate
+  elements. Return a tuple of two arrays that contain indexes of
+  common elements with respect to a1 and a2.
+  """
+  assert a1.dtype == a2.dtype
+  dtype = np.dtype([('item', a1.dtype), ('idx', np.int32)])
+  b = np.empty(a1.size+a2.size, dtype)
+  b['item'][:a1.size] = a1
+  b['item'][a1.size:] = a2
+  b['idx'][:a1.size] = np.arange(a2.size, a2.size+a1.size)
+  b['idx'][a1.size:] = np.arange(a2.size)
+  b.sort(order='item')
+  mask = b[:-1]['item'] == b[1:]['item']
+  return b[1:][mask]['idx'] - a2.size, b[mask]['idx']
---
+++ tests/bl/vl/utils/test_np_ext.py
@@ -0,0 +1,51 @@
+import unittest, time
+import numpy as np
+
+import bl.vl.utils.np_ext as np_ext
+
+
+class TestIndexIntersect(unittest.TestCase):
+
+  def test_simple_array(self):
+    a1 = np.array(list('axzbykcq'))
+    a2 = np.array(list('12ab34'))
+    i1, i2 = np_ext.index_intersect(a1, a2)
+    self.assertEqual(i1.tolist(), [0, 3])
+    self.assertEqual(i2.tolist(), [2, 3])
+
+  def test_record_array(self):
+    a1 = np.array([('V002', 0), ('V068', 1), ('V129', 0)],
+                  dtype=[('marker_vid', '|S34'), ('allele_flip', '|i1')])
+    a2 = np.array([('V%03d' % i, 'foo-%03d' % i) for i in xrange(1000)],
+                  dtype=[('vid', '|S34'), ('label', '|S48')])
+    i1, i2 = np_ext.index_intersect(a1['marker_vid'], a2['vid'])
+    self.assertEqual(i1.tolist(), [0, 1, 2])
+    self.assertEqual(i2.tolist(), [2, 68, 129])
+    self.assertEqual(a1[i1].tolist(), [('V002', 0), ('V068', 1), ('V129', 0)])
+    self.assertEqual(
+      a2[i2].tolist(),
+      [('V002', 'foo-002'), ('V068', 'foo-068'), ('V129', 'foo-129')]
+      )
+
+  def test_performance(self):
+    size = 1000000
+    offset = size/10000
+    a1 = np.array(['V%07d' % i for i in xrange(offset, size)])
+    a2 = np.array(['V%07d' % i for i in xrange(offset+size)])
+    t0 = time.time()
+    i1, i2 = np_ext.index_intersect(a1, a2)
+    print
+    print "finished in %.1f s" % (time.time()-t0)
+
+
+def suite():
+  suite = unittest.TestSuite()
+  suite.addTest(TestIndexIntersect('test_simple_array'))
+  suite.addTest(TestIndexIntersect('test_record_array'))
+  #suite.addTest(TestIndexIntersect('test_performance'))
+  return suite
+
+
+if __name__ == '__main__':
+  runner = unittest.TextTestRunner(verbosity=2)
+  runner.run((suite()))
