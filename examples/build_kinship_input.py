bdf8d64db3cae0d4982ada5881e3aee27dca9a96
Luca Lianas <luca.lianas@crs4.it>
2012-03-29 17:58:21 +0200

    Updated software behaviour.
    One of the input files now maps data samples, not individuals.
    The default behaviour is now to use all data samples, a specific
    parameter has been added in order to use only one data sample for each
    individual (the first one retrieved from omero).

--- examples/build_kinship_input.py
+++ examples/build_kinship_input.py
@@ -8,13 +8,13 @@ BB 00 AA AA AB
 AA AA BB BB AB
 ...
 
-* each colum represents a single individual
+* each colum represents a single data sample
 * each row represents a specific SNP
 
 NOTE WELL: if the output file is too big to be handled with the RAM of
 the computer that runs this script, the file can be written as the
 transposed version of the output format described above (each row will
-represent an individual and each column a SNP) and transposed using a
+represent a data sample and each column a SNP) and transposed using a
 specific map-reduce job.
 
 """
@@ -36,15 +36,16 @@ class KinshipWriter(object):
     TODO: write something....
     """
     def __init__(self, mset, path, base_filename = 'bl-vl-kinship',
-                 transpose_output = False):
+                 transpose_output = False, ignore_duplicated = False):
         self.mset = mset
-        self.out_k_file = open(os.path.join(path, '%s.kinship' % base_filename),
+        self.out_k_file = open(os.path.join(path, '%s.genotypes' % base_filename),
                                'w')
         self.out_k_csvw = csv.writer(self.out_k_file, delimiter='\t')
-        self.out_i_file = open(os.path.join(path, '%s.inds' % base_filename),
+        self.out_ds_file = open(os.path.join(path, '%s.data_samples' % base_filename),
                                'w')
-        self.out_i_csvw = csv.writer(self.out_i_file, delimiter='\t')
+        self.out_ds_csvw = csv.writer(self.out_ds_file, delimiter='\t')
         self.tro = transpose_output
+        self.igd = ignore_duplicated
         self.out_data = []
         self.kb = self.mset.proxy
 
@@ -61,15 +62,18 @@ class KinshipWriter(object):
         if data_collection_samples:
             dsamples = [d for d in dsamples if d in data_collection_samples]
         if len(dsamples) > 0:
-            ds = dsamples[0] # Right now, just ignore all the other data samples
-            self.out_i_csvw.writerow([individual.id])
-            probs, _ = ds.resolve_to_data()
-            disc_probs = [allele_patterns[x]
-                          for x in project_to_discrete_genotype(probs)]
-            if self.tro:
-                self.out_k_csvw.writerow(disc_probs)
-            else:
-                self.out_data.append(disc_probs)
+            if self.igd:
+                dsamples = dsamples[:1]
+            for ds in dsamples:
+                probs, _ = ds.resolve_to_data()
+                if probs is not None:
+                    self.out_ds_csvw.writerow([ds.id])
+                    disc_probs = [allele_patterns[x]
+                                  for x in project_to_discrete_genotype(probs)]
+                    if self.tro:
+                        self.out_k_csvw.writerow(disc_probs)
+                    else:
+                        self.out_data.append(disc_probs)
         
     def close(self):
         """
@@ -80,7 +84,7 @@ class KinshipWriter(object):
             for d in self.out_data:
                 self.out_k_csvw.writerow(d)
         self.out_k_file.close()
-        self.out_i_file.close()
+        self.out_ds_file.close()
         
 
 
@@ -97,6 +101,8 @@ def make_parser():
                         default = None)
     parser.add_argument('--transpose_output', action='store_true',
                         help = 'write the transposed version of the standard output')
+    parser.add_argument('--ignore_duplicated', action='store_true',
+                        help = 'if more than one data sample is connected to an indiviudal use only the first one')
     parser.add_argument('--host', '-H', type = str, help = 'omero hostname',
                         default = 'localhost')
     parser.add_argument('--user', '-U', type = str, help = 'omero user',
@@ -146,7 +152,8 @@ def main(argv):
 
     kw_args = {'mset' : mset,
                'path' : args.out_path,
-               'transpose_output' : args.transpose_output}
+               'transpose_output' : args.transpose_output,
+               'ignore_duplicated' : args.ignore_duplicated}
     if args.base_filename:
         kw_args['base_filename'] = args.base_filename
         
@@ -154,7 +161,9 @@ def main(argv):
     
     logger.info('Writing records')
     for ind in inds:
-        logger.debug('Writing record for individual %s' % ind.id)
+        logger.debug('Writing record for individual %s (%d/%d)' % (ind.id,
+                                                                   inds.index(ind) + 1,
+                                                                   len(inds)))
         kinship_writer.write_record(ind, dc_samples)
 
     logger.info('Closing writer')
