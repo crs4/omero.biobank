2417b6ceffe3f1616464c13c8966f04ac26a15ff
Luca Lianas <luca.lianas@crs4.it>
2012-04-02 14:25:04 +0200

    examples/build_kinship_input.py: print movedo to logger.error

--- examples/build_kinship_input.py
+++ examples/build_kinship_input.py
@@ -149,7 +149,7 @@ def main(argv):
         user = args.user if args.user else ome_user()
         passwd = args.passwd if args.passwd else ome_passwd()
     except ValueError, ve:
-        print ve
+        logger.error(ve)
         sys.exit(2)
 
     kb = KB(driver='omero')(host, user, passwd)
