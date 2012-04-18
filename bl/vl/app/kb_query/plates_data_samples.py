a297ee10043a2ebbe5059aeb0870d389fb683095
Luca Lianas <luca.lianas@crs4.it>
2012-04-18 18:08:38 +0200

    Improved performances when loading data from multiple plates

--- bl/vl/app/kb_query/plates_data_samples.py
+++ bl/vl/app/kb_query/plates_data_samples.py
@@ -3,26 +3,43 @@ import csv, argparse
 from bl.vl.app.importer.core import Core
 
 class BuildPlateDataSamplesDetails(Core):
+
     def __init__(self, host = None, user = None, passwd = None, keep_tokens = 1,
                  logger = None, study_label = None, operator = 'Alfred E. Neumann'):
         super(BuildPlateDataSamplesDetails, self).__init__(host, user, passwd,
                                                            keep_tokens = keep_tokens,
                                                            study_label = study_label,
                                                            logger = logger)
+        self.ACTION_TYPES = [self.kb.ActionOnVessel,
+                             self.kb.ActionOnDataCollectionItem]
+        self.TARGET_TYPES = [self.kb.PlateWell,
+                             self.kb.DataCollectionItem]
 
     def load_plate(self, plate_barcode):
         query = 'SELECT pl FROM TiterPlate pl WHERE pl.barcode = :pl_barcode'
         plate = self.kb.find_all_by_query(query, {'pl_barcode' : plate_barcode})
         return plate[0] if len(plate) > 0 else None
 
-    def get_connected_data_samples(self, well):
-        query = '''SELECT ds FROM DataSample ds
-                   JOIN ds.action action WHERE
-                   action.id IN (SELECT act.id FROM ActionOnVessel act
-                                 JOIN act.target tg
-                                 WHERE tg.vid = :target_id)
-                '''
-        return self.kb.find_all_by_query(query, {'target_id' : well.id})
+    def load_genotype_data_samples_lookup(self):
+        self.logger.info('Loading Data Samples lookup table')
+        acts = []
+        for at in self.ACTION_TYPES:
+            acts.extend(self.kb.get_objects(at))
+        targets = []
+        for tt in self.TARGET_TYPES:
+            targets.extend(self.kb.get_objects(tt))
+        gdsamples = self.kb.get_objects(self.kb.GenotypeDataSample)
+        lookup = {}
+        for gds in gdsamples:
+            lookup.setdefault(gds.action.target, []).append(gds)
+        self.logger.info('Data Samples lookup table loaded')
+        return lookup
+
+    def get_connected_data_samples(self, well, dsamples_lookup):
+        try:
+            return dsamples_lookup[well]
+        except KeyError:
+            return []
 
     def calculate_well_label(self, slot_position, plate_columns):
         if (slot_position % plate_columns) != 0:
@@ -40,51 +57,68 @@ class BuildPlateDataSamplesDetails(Core):
                                                          plate.columns),
                 'WELL_status' : 'UNKNOWN OR EMPTY'}
 
-    def dump(self, plate_barcode, out_file):
-        self.logger.info('Loading plate %s' % plate_barcode)
-        plate = self.load_plate(plate_barcode)
-        if not plate:
-            msg = 'Barcode %s is not related to a known plate' % plate_barcode
+    def dump(self, plate_barcodes, fetch_all, out_file):
+        if not plate_barcodes and not fetch_all:
+            raise ValueError('At least one between the --plate and --fetch_all parameters must be submitted')
+        self.logger.info('Loading plates')
+        if fetch_all:
+            plates = [pl for pl in self.kb.get_objects(self.kb.TiterPlate) \
+                          if pl.barcode]
+        else:
+            plates = []
+            for bc in plate_barcodes.split(','):
+                self.logger.info('Loading plate %s' % bc)
+                plates.append(self.load_plate(bc))
+        if len(plates) == 0:
+            msg = 'No plates found'
             self.logger.critical(msg)
             raise ValueError(msg)
+        else:
+            self.logger.info('Loaded %d plates' % len(plates))
 
-        self.logger.info('Loading wells for plate %s' % plate.barcode)
-        wells = list(self.kb.get_wells_by_plate(plate))
-        self.logger.info('Loaded %d wells' % len(wells))
-        wells_map = {}
-        for w in wells:
-            wells_map[w.slot] = w
+        gds_lookup = self.load_genotype_data_samples_lookup()
+
+        plates_lookup = {}
+        for pl in plates:
+            self.logger.info('Loading wells for plate %s' % pl.barcode)
+            wells = list(self.kb.get_wells_by_plate(pl))
+            self.logger.info('Loaded %d wells' % len(wells))
+            wells_map = {}
+            for w in wells:
+                wells_map[w.slot] = w
+            plates_lookup[pl] = wells_map
 
         writer = csv.DictWriter(out_file, delimiter='\t', restval = 'X',
                                 fieldnames = ['PLATE_barcode', 'PLATE_label',
                                               'WELL_label', 'WELL_status',
                                               'DATA_SAMPLE_label'])
         writer.writeheader()
-        last_slot = 0
-        for slot, well in sorted(wells_map.iteritems()):
-            self.logger.debug('WELL %s --- SLOT %d' % (well.label, slot))
-            while(last_slot < slot - 1):
+        for pl, wmap in plates_lookup.iteritems():
+            last_slot = 0
+            for slot, well in sorted(wmap.iteritems()):
+                self.logger.debug('WELL %s --- SLOT %d' % (well.label, slot))
+                while(last_slot < slot - 1):
+                    last_slot += 1
+                    self.logger.debug('No data for well %s, filling with a dummy record' %
+                                      self.calculate_well_label(last_slot, pl.columns))
+                    writer.writerow(self.get_empty_record(pl, last_slot))
+                record = {'PLATE_barcode' : pl.barcode,
+                          'PLATE_label' : pl.label,
+                          'WELL_label' : well.label,
+                          'WELL_status' : well.status.enum_label()}
+                dsamples = self.get_connected_data_samples(well, gds_lookup)
+                self.logger.info('Retrieved %d data samples for well %s' % (len(dsamples),
+                                                                            well.label))
+                if len(dsamples) > 0:
+                    record['DATA_SAMPLE_label'] = dsamples[0].label
+                writer.writerow(record)
+                last_slot = slot
+            # Fill empty records if last_slot != plate.rows * plate.columns
+            while (last_slot != (pl.rows * pl.columns)):
                 last_slot += 1
                 self.logger.debug('No data for well %s, filling with a dummy record' %
-                                  self.calculate_well_label(last_slot, plate.columns))
-                writer.writerow(self.get_empty_record(plate, last_slot))
-            record = {'PLATE_barcode' : plate.barcode,
-                      'PLATE_label' : plate.label,
-                      'WELL_label' : well.label,
-                      'WELL_status' : well.status.enum_label()}
-            dsamples = self.get_connected_data_samples(well)
-            self.logger.info('Retrieved %d data samples for well %s' % (len(dsamples),
-                                                                        well.label))
-            if len(dsamples) > 0:
-                record['DATA_SAMPLE_label'] = dsamples[0].label
-            writer.writerow(record)
-            last_slot = slot
-        # Fill empty records if last_slot != plate.rows * plate.columns
-        while (last_slot != (plate.rows * plate.columns)):
-            last_slot += 1
-            self.logger.debug('No data for well %s, filling with a dummy record' %
-                              self.calculate_well_label(last_slot, plate.columns))
-            writer.writerow(self.get_empty_record(plate, last_slot))
+                                  self.calculate_well_label(last_slot, pl.columns))
+                writer.writerow(self.get_empty_record(pl, last_slot))
 
         out_file.close()
         self.logger.info('Job completed')
@@ -95,15 +129,16 @@ connected to them.
 """
 
 def make_parser(parser):
-    parser.add_argument('-p', '--plate', type=str, required=True,
-                        help = 'plate\'s barcode')
+    parser.add_argument('-p', '--plates', type=str, help = 'one or more barcodes separated with a comma')
+    parser.add_argument('--fetch_all', action='store_true',
+                        help='retrieve all plates with a barcode, this parameter overrides the --plate')
 
 def implementation(logger, host, user, passwd, args):
     app = BuildPlateDataSamplesDetails(host = host, user = user,
                                        passwd = passwd, study_label = None,
                                        keep_tokens = args.keep_tokens,
                                        logger = logger)
-    app.dump(args.plate, args.ofile)
+    app.dump(args.plates, args.fetch_all, args.ofile)
 
 def do_register(registration_list):
     registration_list.append(('plate_data_samples', help_doc, make_parser,
