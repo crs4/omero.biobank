27f7c62f0f7e6cc06c185e8deecd5f61b5afea38
Luca Lianas <luca.lianas@crs4.it>
2012-04-19 16:50:20 +0200

    Added new models

--- bl/vl/kb/drivers/omero/vessels.py
+++ bl/vl/kb/drivers/omero/vessels.py
@@ -6,7 +6,8 @@ import omero.rtypes as ort
 
 import wrapper as wp
 from action import Action
-from utils import assign_vid_and_timestamp, make_unique_key
+from utils import assign_vid_and_timestamp, make_unique_key, assign_vid
+from objects_collections import VLCollection
 from objects_collections import TiterPlate
 
 
@@ -75,3 +76,32 @@ class PlateWell(Vessel):
     csi_uk = make_unique_key(self.container.label, '%04d' % self.slot)
     setattr(self.ome_obj, 'containerSlotIndexUK',
             self.to_omero(self.__fields__['containerSlotIndexUK'][0], csi_uk))
+
+
+# These classes are not in objects_collections module in order to
+# prevent a cyclic import issue
+class VesselsCollection(VLCollection):
+  
+  OME_TABLE = 'VesselsCollection'
+  __fields__ = []
+
+
+class VesselsCollectionItem(wp.OmeroWrapper):
+
+  OME_TABLE = 'VesselsCollectionItem'
+  __fields__ = [('vid', wp.VID, wp.REQUIRED),
+                ('vessel', Vessel, wp.REQUIRED),
+                ('vesselsCollection', VesselsCollection, wp.REQUIRED),
+                ('vesselsCollectionItemUK', wp.STRING, wp.REQUIRED)]
+
+  def __preprocess_conf__(self, conf):
+    if not 'vesselsCollectionItemUK' in conf:
+      v_vid = conf['vessel'].id
+      vc_vid = conf['vesselsCollection'].id
+      conf['vesselsCollectionItemUK'] = make_unique_key(vc_vid, v_vid)
+    return assign_vid(conf)
+
+  def __update_contraints__(self):
+    vci_uk = make_unique_key(self.vesselsCollection.id, self.vessel.id)
+    setattr(self.ome_obj, 'vesselsCollectionItemUK',
+            self.to_omero(self.__fields__['vesselsCollectionItemUK'][0], vci_uk))
