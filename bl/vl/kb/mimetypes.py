fed8a41ae8b57a9a321d63ab2fd944b18e5a2c47
Luca Lianas <luca.lianas@crs4.it>
2012-07-13 18:04:44 +0200

    New mimetypes for sequencing related files

--- bl/vl/kb/mimetypes.py
+++ bl/vl/kb/mimetypes.py
@@ -4,5 +4,11 @@
 GDO_TABLE = 'x-bl/gdo-table'
 SSC_FILE = 'x-ssc-messages'
 CEL_FILE = 'x-vl/affymetrix-cel'
+SAM_FILE = 'x-vl/sam-file'
+BAM_FILE = 'x-vl/bam-file'
+QSEQ_FILE = 'x-vl/qseq-file'
+FASTQ_FILE = 'x-vl/fastq-file'
+BCL_FILE = 'x-vl/illumina-bcl'
 
-DATA_OBJECT_FILES = [SSC_FILE, CEL_FILE]
+DATA_OBJECT_FILES = [SSC_FILE, CEL_FILE, SAM_FILE, BAM_FILE,
+                     QSEQ_FILE, FASTQ_FILE, BCL_FILE]
