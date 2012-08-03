db2e5074461e90691a3abe282b6b924fe5eb7b0e
simleo <simleo@crs4.it>
2012-08-03 14:09:39 +0200

    docs: added a directory for Sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ PARENT_DIR = os.path.split(THIS_DIR)[0]
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('_ext'))
 
 # -- General configuration -----------------------------------------------------
 
