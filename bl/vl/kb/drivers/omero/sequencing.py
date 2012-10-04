888a77e12201b2334f81229c1bd7c2778b942704
Luca Lianas <luca.lianas@crs4.it>
2012-10-04 11:14:20 +0200

    Models revision

--- bl/vl/kb/drivers/omero/sequencing.py
+++ bl/vl/kb/drivers/omero/sequencing.py
@@ -3,7 +3,7 @@ import omero.rtypes as ort
 
 import wrapper as wp
 from data_samples import DataSample
-from vessels import Vessel
+from vessels import Tube
 from objects_collections import Lane
 
 
@@ -11,14 +11,11 @@ class SequencerOutput(DataSample):
   OME_TABLE = 'SequencerOutput'
   __fields__ = []
 
-class RawSeqDataSample(DataSample):
-  
+class RawSeqDataSample(DataSample):  
   OME_TABLE = 'RawSeqDataSample'
-  __fields__ = [('read', wp.INT, wp.REQUIRED),
-                ('lane', Lane, wp.REQUIRED)]
+  __fields__ = [('lane', Lane, wp.OPTIONAL)]
 
 
-class SeqDataSample(DataSample):
-  
+class SeqDataSample(DataSample):  
   OME_TABLE = 'SeqDataSample'
-  __fields__ = [('sample', Vessel, wp.REQUIRED)]
+  __fields__ = [('sample', Tube, wp.OPTIONAL)]
