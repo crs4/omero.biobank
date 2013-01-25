710f420241a527efbbd54ad4b815de3a6d04cc8b
simleo <simleo@crs4.it>
2013-01-25 16:19:59 +0100

    kb: fixed test_table

--- test/kb/drivers/omero/test_table.py
+++ test/kb/drivers/omero/test_table.py
@@ -16,6 +16,8 @@ RES_SIZE = 256
 VID_SIZE = 64
 ARRAY_SIZE = 64
 
+N_ROWS = 16
+
 
 def get_random_table_name():
   return '%s.h5' % uuid.uuid4().hex
@@ -36,18 +38,18 @@ class TestProxyCore(unittest.TestCase):
       ]
     return fields
 
-  def __fill_table(self, pc, table_name, N):
-    rec_type = pc.get_table_headers(table_name)
-    data = np.zeros(N, dtype=rec_type)
-    data['r_id'] =  np.arange(N)
-    data['r_type'] = ['r_type%04d' % i for i in xrange(N)]
-    data['r_vid'] = ['r_vid%04d' % i for i in xrange(N)]
-    data['o_vid'] = ['o_vid%04d' % i for i in xrange(N)]
-    data['t_vid'] = ['t_vid%04d' % i for i in xrange(N)]
-    data['i_vid'] = ['i_vid%04d' % i for i in xrange(N)]
-    data['a_d_type'] = [np.random.random(ARRAY_SIZE) for i in xrange(N)]
+  def __fill_table(self, pc, table_name, n_rows):
+    rec_desc = pc.get_table_headers(table_name)
+    data = np.zeros(n_rows, dtype=rec_desc)
+    data['r_id'] =  np.arange(n_rows)
+    data['r_type'] = ['r_type%04d' % i for i in xrange(n_rows)]
+    data['r_vid'] = ['r_vid%04d' % i for i in xrange(n_rows)]
+    data['o_vid'] = ['o_vid%04d' % i for i in xrange(n_rows)]
+    data['t_vid'] = ['t_vid%04d' % i for i in xrange(n_rows)]
+    data['i_vid'] = ['i_vid%04d' % i for i in xrange(n_rows)]
+    data['a_d_type'] = [np.random.random(ARRAY_SIZE) for i in xrange(n_rows)]
     data['a_l_type'] = [
-      np.random.randint(0, 2**63-1, ARRAY_SIZE) for i in xrange(N)
+      np.random.randint(0, 2**63-1, ARRAY_SIZE) for i in xrange(n_rows)
       ]
     pc.add_table_rows(table_name, data[:-1])
     pc.add_table_row(table_name, data[-1])
@@ -73,73 +75,75 @@ class TestProxyCore(unittest.TestCase):
     finally:
       pc.delete_table(table_name)
 
-  def test_get_table_rows(self):
-    fields = self.__make_fields()
-    table_name = get_random_table_name()
-    N = 16
-    pc = ProxyCore(OME_HOST, OME_USER, OME_PASS)
-    pc.create_table(table_name, fields)
-    data = self.__fill_table(pc, table_name, N)
-    irange = range(N/4, (3*N)/4)
-    selectors = ['(r_vid == "%s")' % data[i]['r_vid'] for i in irange]
-    sel_classic = '|'.join(selectors)
-    rows_classic = pc.get_table_rows(table_name, selector=sel_classic)
-    for i, r in it.izip(irange, rows_classic):
-      self.assertTrue(data[i] == r)
-    sel_lite    = selectors
-    rows_lite = pc.get_table_rows(table_name, selector=sel_lite)
-    for i, r in it.izip(irange, rows_classic):
-      self.assertTrue(data[i] == r)
-
-  def test_update_row(self):
-    fields = self.__make_fields()
-    table_name = get_random_table_name()
-    N = 16
-    pc = ProxyCore(OME_HOST, OME_USER, OME_PASS)
-    pc.create_table(table_name, fields)
-    data = self.__fill_table(pc, table_name, N)
-    r = pc.get_table_rows(table_name, None)
-    self.assertTrue(np.all(data == r))
-    m = N/2
-    r = pc.get_table_rows(table_name, '(r_vid == "%s")' % data[m]['r_vid'])
-    self.assertEqual(len(r), 1)
-    self.assertTrue(data[m] == r[0])
-    urow = data[m].copy()
-    urow['o_vid'] = 'foo' + urow['o_vid']
-    r = pc.get_table_rows(table_name, '(r_vid == "%s")' % data[m]['r_vid'])
-    self.assertEqual(len(r), 1)
-    pc.update_table_row(table_name, '(r_vid == "%s")' % data[m]['r_vid'], urow)
-    r = pc.get_table_rows(table_name, '(r_vid == "%s")' % data[m]['r_vid'])
-    self.assertEqual(len(r), 1)
-    self.assertTrue(urow == r[0])
-    pc.delete_table(table_name)
-
   def test_table_rows(self):
     fields = self.__make_fields()
     table_name = get_random_table_name()
-    N = 16
-    pc = ProxyCore(OME_HOST, OME_USER, OME_PASS)
-    pc.create_table(table_name, fields)
-    data = self.__fill_table(pc, table_name, N)
-    r = pc.get_table_rows(table_name, None)
-    self.assertTrue(np.all(data == r))
-    m = N/2
-    r = pc.get_table_rows(table_name, '(r_vid == "%s")' % data[m]['r_vid'])
-    self.assertEqual(len(r), 1)
-    self.assertTrue(data[m] == r[0])
-    pc.delete_table(table_name)
+    try:
+      pc = ProxyCore(OME_HOST, OME_USER, OME_PASS)
+      pc.create_table(table_name, fields)
+      data = self.__fill_table(pc, table_name, N_ROWS)
+      rows = pc.get_table_rows(table_name, None)
+    finally:
+      pc.delete_table(table_name)
+    self.assertTrue(np.all(data == rows))
 
   def test_table_rows_iterator(self):
     fields = self.__make_fields()
     table_name = get_random_table_name()
-    N = 16
-    pc = ProxyCore(OME_HOST, OME_USER, OME_PASS)
-    pc.create_table(table_name, fields)
-    data = self.__fill_table(pc, table_name, N)
-    row_it = pc.get_table_rows_iterator(table_name)
+    try:
+      pc = ProxyCore(OME_HOST, OME_USER, OME_PASS)
+      pc.create_table(table_name, fields)
+      data = self.__fill_table(pc, table_name, N_ROWS)
+      row_it = pc.get_table_rows_iterator(table_name)
+    finally:
+      pc.delete_table(table_name)
     for i, row in enumerate(row_it):
       self.assertTrue(row == data[i])
-    pc.delete_table(table_name)
+
+  def test_update_row(self):
+    fields = self.__make_fields()
+    table_name = get_random_table_name()
+    try:
+      pc = ProxyCore(OME_HOST, OME_USER, OME_PASS)
+      pc.create_table(table_name, fields)
+      data = self.__fill_table(pc, table_name, N_ROWS)
+      r = pc.get_table_rows(table_name, None)
+      self.assertTrue(np.all(data == r))
+      m = N_ROWS/2
+      urow = data[m].copy()
+      urow['o_vid'] = 'foo' + urow['o_vid']
+      pc.update_table_row(
+        table_name, '(r_vid == "%s")' % data[m]['r_vid'], urow
+        )
+      r = pc.get_table_rows(table_name, '(r_vid == "%s")' % data[m]['r_vid'])
+    finally:
+      pc.delete_table(table_name)
+    self.assertEqual(len(r), 1)
+    self.assertTrue(urow == r[0])
+
+  def test_selections(self):
+    fields = self.__make_fields()
+    table_name = get_random_table_name()
+    try:
+      pc = ProxyCore(OME_HOST, OME_USER, OME_PASS)
+      pc.create_table(table_name, fields)
+      data = self.__fill_table(pc, table_name, N_ROWS)
+      M = N_ROWS/2
+      r = pc.get_table_rows(table_name, '(r_vid == "%s")' % data[M]['r_vid'])
+      irange = xrange(N_ROWS/4, (3*N_ROWS)/4)
+      selectors = ['(r_vid == "%s")' % data[i]['r_vid'] for i in irange]
+      sel_classic = '|'.join(selectors)
+      rows_classic = pc.get_table_rows(table_name, selector=sel_classic)
+      sel_lite = selectors
+      rows_lite = pc.get_table_rows(table_name, selector=sel_lite)
+    finally:
+      pc.delete_table(table_name)
+    self.assertEqual(len(r), 1)
+    self.assertTrue(data[M] == r[0])
+    for i, r in it.izip(irange, rows_classic):
+      self.assertTrue(data[i] == r)
+    for i, r in it.izip(irange, rows_lite):
+      self.assertTrue(data[i] == r)
 
 
 def suite():
@@ -147,8 +151,8 @@ def suite():
   suite.addTest(TestProxyCore('test_create_delete'))
   suite.addTest(TestProxyCore('test_table_rows'))
   suite.addTest(TestProxyCore('test_table_rows_iterator'))
-  suite.addTest(TestProxyCore('test_get_table_rows'))
   suite.addTest(TestProxyCore('test_update_row'))
+  suite.addTest(TestProxyCore('test_selections'))
   return suite
 
 
