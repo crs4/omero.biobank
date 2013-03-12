0a5a8e4f991ef876be5eceabdfae8537a225d3d5
simleo <simleo@crs4.it>
2013-03-12 17:06:20 +0100

    test/genotype/test_{ped,vcf}_writer.py: cleanup

--- test/genotype/test_ped_writer.py
+++ test/genotype/test_ped_writer.py
@@ -1,42 +1,48 @@
 # BEGIN_COPYRIGHT
 # END_COPYRIGHT
 
-# FIXME: it assumes to be launched after ../tools/importers/test_gdo_workflow.sh
+# FIXME:
+#  1. it assumes to be launched after ../tools/importers/test_gdo_workflow.sh
+#  2. no consistency check on written data
 
-import unittest
+import unittest, tempfile, os
 
 from bl.vl.kb import KnowledgeBase as KB
 from bl.vl.genotype.io import PedWriter
 
-STUDY_LABEL='GDO_TEST_STUDY'
-MS_LABEL='GDO_TEST_MS'
-REF_GENOME='hg19'
+STUDY_LABEL = 'GDO_TEST_STUDY'
+MS_LABEL = 'GDO_TEST_MS'
+REF_GENOME = 'hg19'
 
 
-class ped_writer(unittest.TestCase):
+class TestPedWriter(unittest.TestCase):
 
   def setUp(self):
     self.kb = KB(driver='omero')('localhost', 'root', 'romeo')
+    self.wd = tempfile.mkdtemp(prefix="biobank_")
 
   def tearDown(self):
-    pass
+    self.kb.disconnect()
+
+  def __extract_data_sample(self, group, mset, dsample_name):
+    by_individual = {}
+    for i in self.kb.get_individuals(group):
+      gds = filter(lambda x: x.snpMarkersSet == mset,
+                   self.kb.get_data_samples(i, dsample_name))
+      assert(len(gds) == 1)
+      by_individual[i.id] = gds[0]
+    return by_individual
 
   def test_base(self):
-    def extract_data_sample(group, mset, dsample_name):
-      by_individual = {}
-      for i in self.kb.get_individuals(group):
-        gds = filter(lambda x: x.snpMarkersSet == mset,
-                     self.kb.get_data_samples(i, dsample_name))
-        assert(len(gds) == 1)
-        by_individual[i.id] = gds[0]
-      return by_individual
-
+    base_path = os.path.join(self.wd, "test")
+    print "\nwriting to %s*" % base_path
     study = self.kb.get_study(STUDY_LABEL)
     family = self.kb.get_individuals(study)
     mset = self.kb.get_snp_markers_set(label=MS_LABEL)
-    gds_by_individual = extract_data_sample(study, mset, 'GenotypeDataSample')
-
-    pw = PedWriter(mset, base_path="./foo", ref_genome=REF_GENOME)
+    gds_by_individual = self.__extract_data_sample(
+      study, mset, 'GenotypeDataSample'
+      )
+    pw = PedWriter(mset, base_path=base_path, ref_genome=REF_GENOME)
     pw.write_map()
     pw.write_family(study.id, family, gds_by_individual)
     pw.close()
@@ -44,7 +50,7 @@ class ped_writer(unittest.TestCase):
 
 def suite():
   suite = unittest.TestSuite()
-  suite.addTest(ped_writer('test_base'))
+  suite.addTest(TestPedWriter('test_base'))
   return suite
 
 
--- test/genotype/test_vcf_writer.py
+++ test/genotype/test_vcf_writer.py
@@ -1,49 +1,53 @@
 # BEGIN_COPYRIGHT
 # END_COPYRIGHT
 
-# FIXME: it assumes to be launched after ../tools/importers/test_gdo_workflow.sh
+# FIXME:
+#  1. it assumes to be launched after ../tools/importers/test_gdo_workflow.sh
+#  2. no consistency check on written data
 
-import unittest
+import unittest, tempfile, os
 
 from bl.vl.kb import KnowledgeBase as KB
 from bl.vl.genotype.io import VCFWriter
 
 
-STUDY_LABEL='GDO_TEST_STUDY'
-MS_LABEL='GDO_TEST_MS'
-REF_GENOME='hg19'
+STUDY_LABEL = 'GDO_TEST_STUDY'
+MS_LABEL = 'GDO_TEST_MS'
+REF_GENOME = 'hg19'
 
-class vcf_writer(unittest.TestCase):
+
+class TestVCFWriter(unittest.TestCase):
 
   def setUp(self):
     self.kb = KB(driver='omero')('localhost', 'root', 'romeo')
+    self.wd = tempfile.mkdtemp(prefix="biobank_")
 
   def tearDown(self):
-    pass
+    self.kb.disconnect()
+
+  def __extract_data_sample(self, group, mset, dsample_name):
+    dsamples = []
+    for i in self.kb.get_individuals(group):
+      gds = filter(lambda x: x.snpMarkersSet == mset,
+                   self.kb.get_data_samples(i, dsample_name))
+      assert(len(gds) == 1)
+      dsamples.append(gds[0])
+    return dsamples
 
   def test_base(self):
-    def extract_data_sample(group, mset, dsample_name):
-      dsamples = []
-      for i in self.kb.get_individuals(group):
-        gds = filter(lambda x: x.snpMarkersSet == mset,
-                     self.kb.get_data_samples(i, dsample_name))
-        assert(len(gds) == 1)
-        dsamples.append(gds[0])
-      return dsamples
-
+    base_path = os.path.join(self.wd, "test")
+    print "\nwriting to %s*" % base_path
     study = self.kb.get_study(STUDY_LABEL)
-    family = self.kb.get_individuals(study)
     mset = self.kb.get_snp_markers_set(label=MS_LABEL)
-    dsamples = extract_data_sample(study, mset, 'GenotypeDataSample')
-
+    dsamples = self.__extract_data_sample(study, mset, 'GenotypeDataSample')
     vcfw = VCFWriter(mset, ref_genome=REF_GENOME)
-    with open('foo.vcf', 'w') as fo:
+    with open('%s.vcf' % base_path, 'w') as fo:
       vcfw.write(fo, dsamples)
 
 
 def suite():
   suite = unittest.TestSuite()
-  suite.addTest(vcf_writer('test_base'))
+  suite.addTest(TestVCFWriter('test_base'))
   return suite
 
 
