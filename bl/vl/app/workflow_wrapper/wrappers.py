f55ff22316c078be7c91eb95ec55da619fdde50b
Luca Lianas <lianas@crs4.it>
2013-03-28 18:09:37 +0100

    Accept logger as input

--- bl/vl/app/workflow_wrapper/wrappers.py
+++ bl/vl/app/workflow_wrapper/wrappers.py
@@ -4,7 +4,8 @@ import logging
 LOG_FORMAT = '%(asctime)s|%(levelname)-8s|%(message)s'
 LOG_DATEFMT = '%Y-%m-%d %H:%M:%S'
 
-def get_logger(log_format, log_date_format, log_level, filename):
+def get_logger(log_format, log_date_format, log_level,
+               filename = None):
     loglevel = getattr(logging, log_level)
     kwargs = {'format'  : log_format,
               'datefmt' : log_date_format,
@@ -18,9 +19,9 @@ def get_logger(log_format, log_date_format, log_level, filename):
 
 def run_datasets_import(history, items, action_context, async = False,
                         driver = 'galaxy', conf_file_path = None,
-                        log_format = LOG_FORMAT, log_date_format = LOG_DATEFMT,
-                        log_level = 'INFO', log_file = None):
-    logger = get_logger(log_format, log_date_format, log_level, log_file)
+                        logger = None):
+    if not logger:
+        logger = get_logger(LOG_FORMAT, LOG_DATEFMT, 'INFO')
     # Consistency check for items
     if len(items) == 0:
         msg = 'Empty list, nothing to import'
@@ -45,10 +46,9 @@ def run_datasets_import(history, items, action_context, async = False,
 def run_flowcell_from_samplesheet_import(samplesheet_data, action_context,
                                          namespace = None, async = False,
                                          driver = 'galaxy', conf_file_path = None,
-                                         log_format = LOG_FORMAT,
-                                         log_date_format = LOG_DATEFMT,
-                                         log_level = 'INFO', log_file = None):
-    logger = get_logger(log_format, log_date_format, log_level, log_file)
+                                         logger = None):
+    if not logger:
+        logger = get_logger(LOG_FORMAT, LOG_DATEFMT, 'INFO')
     if driver == 'galaxy':
         gw = GalaxyWrapper(conf_file_path, logger)
         gw.run_flowcell_from_samplesheet_import(samplesheet_data, action_context,
