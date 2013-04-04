c9f494c4bdf6cc0e1bdd5b741e3cb6b3a7195d0a
simleo <simleo@crs4.it>
2013-04-04 16:53:03 +0200

    updated create_markers_sets example

--- examples/create_markers_sets.py
+++ examples/create_markers_sets.py
@@ -3,12 +3,10 @@
 
 """ ..
 
-The goal of this example is to show how Markers and SNPMarkersSet are
-defined.
+The goal of this example is to show how marker sets are defined.
 
-We will assume that, as explained in create_individuals.py, we already
-have a set of individuals in omero and that they are enrolled in
-'TEST01'.
+We will assume that, as explained in create_individuals.py, the kb
+already contains a set of individuals enrolled in a study.
 
 For each individual, we have a sample of DNA in a well of a work
 plate, and we assume that we have run a series of TaqMan genotyping
@@ -16,7 +14,7 @@ assays that associates to each well a collection of genotyping
 values. This is, of course, a simplistic model but it should be
 enough to illustrate the procedure.
 
-**Note:** DO NOT run this examples against a production
+**Note:** DO NOT run this example on a production
   database. Also, what will be shown here is supposed to be
   didactically clear, not efficient. See the implementation of the
   importer tools for more efficient solutions.
@@ -25,101 +23,79 @@ First, as usual, we open the connection to the KnowledgeBase
 
 """
 
-import os
+import os, uuid
 import numpy as np
 from bl.vl.kb import KnowledgeBase as KB
-
 
 OME_HOST   = os.getenv('OME_HOST', 'localhost')
 OME_USER   = os.getenv('OME_USER', 'test')
 OME_PASSWD = os.getenv('OME_PASSWD', 'test')
 
+STUDY_LABEL = os.getenv('STUDY_LABEL', 'TEST01')
+
 kb = KB(driver='omero')(OME_HOST, OME_USER, OME_PASSWD)
 
 """ ..
 
-The following is the collection of (fake) markers used in the TaqMan assays.
+The following is the collection of (fake) marker data, where tuple
+elements represent the label, mask, index and allele flip information
+for each marker (see the marker set importer for more info).
 
 """
 
 taq_man_markers = [
-  ('A001', 'xrs122652',  'TCACTTCTTCAAAGCT[A/G]AGCTACAAGCATTATT'),
-  ('A002', 'xrs741592',  'GGAAGGAAGAAATAAA[C/G]CAGCACTATGTCTGGC'),
-  ('A003', 'xrs807079',  'CCGACCTAGTAGGCAA[A/G]TAGACACTGAGGCTGA'),
-  ('A004', 'xrs567736',  'AGGTCTATGTTAATAC[A/G]GAATCAGTTTCTCACC'),
-  ('A005', 'xrs4693427', 'AGATTACCATGCAGGA[A/T]CTGTTCTGAGATTAGC'),
-  ('A006', 'xrs4757019', 'TCTACCTCTGTGACTA[C/G]AAGTGTTCTTTTATTT'),
-  ('A007', 'xrs7958813', 'AAGGCAATACTGTTCA[C/T]ATTGTATGGAAAGAAG')
+  ('A001', 'TCACTTCTTCAAAGCT[A/G]AGCTACAAGCATTATT', 0, False),
+  ('A002', 'GGAAGGAAGAAATAAA[C/G]CAGCACTATGTCTGGC', 1, False),
+  ('A003', 'CCGACCTAGTAGGCAA[A/G]TAGACACTGAGGCTGA', 2, False),
+  ('A004', 'AGGTCTATGTTAATAC[A/G]GAATCAGTTTCTCACC', 3, True),
+  ('A005', 'AGATTACCATGCAGGA[A/T]CTGTTCTGAGATTAGC', 4, False),
+  ('A006', 'TCTACCTCTGTGACTA[C/G]AAGTGTTCTTTTATTT', 5, True),
+  ('A007', 'AAGGCAATACTGTTCA[C/T]ATTGTATGGAAAGAAG', 6, True),
   ]
 
 """ ..
 
-The first element of a marker defining tuple is its label, the second
-is the dbSNP db label, if available, while the third is the marker mask.
-
-.. todo::
-
-  put a reference to reference documentation
-
-Now we will load the markers set definition into Omero.biobank.
-
-**Note:** We are considering an ideal case where none of the markers
-  is already in the db.
+Now we will load the above definitions into the kb:
 
 """
 
-study = kb.get_study('TEST01')
+study = kb.get_study(STUDY_LABEL)
 
 action = kb.create_an_action(study, doc='importing markers')
 action.reload()
 
-source, context, release = 'foobar', 'fooctx', 'foorel'
-ref_rs_genome, dbsnp_build = 'foo-rs-genome', 13200
+label, maker, model, release = (uuid.uuid4().hex for _ in xrange(4))
+N, stream = len(taq_man_markers), iter(taq_man_markers)
 
-lvs = kb.create_markers(source, context, release, ref_rs_genome, dbsnp_build,
-                        taq_man_markers, action)
+mset = kb.create_snp_markers_set(
+  label, maker, model, release, N, stream, action
+  )
 
 """ ..
-
-where lvs is a list of (label, vid) tuples.
 
 We can assume that the markers above have been aligned against a
-reference genome, say fake19, and save in omero.biobank the alignment
-information.  We will fake the alignment results as follows.
+reference genome, and save the alignment information. The latter
+consists of a stream of tuples where each tuple is in the marker vid,
+chromosome number, position within the chromosome, strand info, number
+of copies (see the docs for ``kb.align_snp_markers_set``):
 
 """
 
-aligns = [(v[1], 1, i*1000, True, 'A' if (i%2)== 0 else 'B', 1)
-          for i, v in enumerate(lvs)]
-
+mset.load_markers()
+aligns = [(m['vid'], i+1, (i+1)*1000, True, 'A' if (i%2)== 0 else 'B', 1)
+          for i, m in enumerate(mset.markers)]
 ref_genome = 'fake19'
-kb.save_snp_markers_alignments(ref_genome, aligns, action)
-
-""" ..
-
-We can now create an actual markers set.
-
-"""
-
-taq_man_set = [(v[1], i, False) for i, v in enumerate(lvs)]
-label, maker, model, release = 'FakeTaqSet01', 'CRS4', 'TaqManSet', '23/09/2011'
-
-mset = kb.create_snp_markers_set(label, maker, model, release,
-                                 taq_man_set, action)
+kb.align_snp_markers_set(mset, ref_genome, iter(aligns), action)
 
 """ ...
 
 The function below will provide us with the data needed to fake the
-results of a genotyping assay on the given SNPMarkersSet.  As
-discussed in XXXX, we need a numpy array of shape '''(2, N)''', where
-'''N''' is the number of markers, that represents the probability of
-being homozygous on allele A and allele B, and a numpy array of shape
-'''(N,)''' that represent the relative confidence on the measurements.
-
-.. todo::
-
-   write a general discussion on the floating point approach to
-   genotyping data storage.
+results of a genotyping assay on the given SNPMarkersSet.  This is
+modeled by a 2 X N (N = number of markers) array where each column
+represents the probabilities of being homozygous on allele A and B,
+and a 1 X N array where each element represents a measure of
+confidence related to the measure contained at the same position in
+the probabilities array.
 
 """
 
@@ -132,63 +108,38 @@ def make_fake_data(mset):
 """ ..
 
 Now that we have the SNPMarkersSet, we can proceed with the definition
-of the datasets. We will consider a minimalistic model, where we will
+of the datasets. We will consider a minimalistic model, where we do
 not keep track of the Tube, PlateWell, ... chain that goes from the
 individual to the experiment, but we directly link the dataset to the
-individuals. To register this dependency and how the dependency was
-established, each DataSample object is linked with an action to the
-the relevant Individual instance. Mainly for historical reasons, the
-action is thought as an arrow that starts from the 'newer' object, the
-GenotypeDataSample instance in this case, and has as a 'target' the
-object on which the process operated. It is, essentially, a
-representation of the 'inverse' of the creation process.
-
-.. todo::
-
-   uhmmm, not sure that the Action explanation above is crystal clear :-).
+individuals.
 
 """
 
-mset.load_markers()
-kb.update_snp_positions(mset.markers, ref_genome)
-data_sample_by_id = {}
-family = []
+data_sample_list = []
 for i, ind in enumerate(kb.get_individuals(study)):
-  family.append(ind)
   action = kb.create_an_action(study, target=ind, doc='fake dataset')
-  conf = {'label' : 'taq-%03d' % i,
+  conf = {'label' : uuid.uuid4().hex,
           'status' : kb.DataSampleStatus.USABLE,
           'action' : action,
           'snpMarkersSet' : mset}
   data_sample = kb.factory.create(kb.GenotypeDataSample, conf).save()
   probs, conf = make_fake_data(mset)
   do = kb.add_gdo_data_object(action, data_sample, probs, conf)
-  data_sample_by_id[ind.id] = data_sample
+  data_sample_list.append(data_sample)
 
 """ ..
 
-Note how we first create a DataSample object (GenotypeDataSample)
-which basically keeps track of the fact that there exists a genotyping
-data set defined on a given snp markers set, and then we provide an
-actual data object that describes the physical object that contains
-the real data. The idea is that there could be many instances, data
-equivalent, that link to the same DataSample, e.g., on different file
-systems, in different formats and so on.
+Note how we first create a ''data sample'' (GenotypeDataSample), which
+keeps track of the existence of genotyping data defined on a given
+marker set, and then we provide a ''data object'' that models the
+physical object that actually contains the data. Multiple data objects
+can link to the same data sample (they would contain the same data,
+but on different file systems, or encoded in different formats).
 """
 
-""" ..
-
-As an example, we will now write out the information we have just
-saved as a plink pedfile.
-
-"""
-from bl.vl.genotype.io import PedWriter
-
-pw = PedWriter(mset, base_path="/tmp/foo")
-
-pw.write_map()
-
-family_label = study.label
-pw.write_family(family_label, family, data_sample_by_id)
-
-pw.close()
+np.set_printoptions(precision=3)
+print "marker set %s" % mset.id
+for gdo in kb.get_gdo_iterator(mset, data_samples=data_sample_list):
+  print gdo['probs']
+  print gdo['confidence']
+  print
