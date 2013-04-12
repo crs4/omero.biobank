31130388b93aaec19aad8ebf3eb7608f6e8063e7
Luca Lianas <lianas@crs4.it>
2013-04-12 16:10:47 +0200

    massive code refactory and added messages validation

--- bl/vl/kb/events.py
+++ bl/vl/kb/events.py
@@ -1,5 +1,82 @@
 import json
+from voluptuous import Schema
 from bl.vl.kb.drivers.omero.utils import ome_hash
+from bl.vl.utils import decode_dict
+
+
+def build_event(event_cls, event_conf):
+    def get_node_creation_data(conf):
+        return {
+            'action': 'NODE_CREATION',
+            'details': {
+                'obj_class': type(conf['bl_obj']).__name__,
+                'obj_id': conf['bl_obj'].id,
+                'obj_hash': ome_hash(conf['bl_obj'].ome_obj)
+            }
+        }
+
+    def get_edge_creation_data(conf):
+        return {
+            'action': 'EDGE_CREATE',
+            'details': {
+                'act_type': type(conf['bl_act']).__name__,
+                'act_id': conf['bl_act'].id,
+                'act_hash': ome_hash(conf['bl_act'].ome_obj)
+            },
+            'source_node': ome_hash(conf['bl_src_obj'].ome_obj),
+            'dest_node': ome_hash(conf['bl_dest_obj'].ome_obj)
+        }
+
+    def get_node_deletion_data(conf):
+        return {
+            'action': 'NODE_DELETE',
+            'target': ome_hash(conf['bl_obj'].ome_obj)
+        }
+
+    def get_edge_deletion_data(conf):
+        return {
+            'action': 'EDGE_DELETE',
+            'target': ome_hash(conf['bl_act'].ome_obj)
+        }
+
+    def get_edge_update_data(conf):
+        data = {
+            'action': 'EDGE_UPDATE',
+            'target': ome_hash(conf['bl_act'].ome_obj),
+            'new_source_node': None,
+            'new_dest_node': None
+        }
+        if conf['bl_src_obj']:
+            data['new_source_node'] = ome_hash(conf['bl_src_obj'].ome_obj)
+        if conf['bl_dest_obj']:
+            data['new_dest_node'] = ome_hash(conf['bl_dest_obj'].ome_obj)
+        return data
+
+    get_data_map = {
+        NodeCreationEvent: get_node_creation_data,
+        EdgeCreationEvent: get_edge_creation_data,
+        NodeDeletionEvent: get_node_deletion_data,
+        EdgeDeletionEvent: get_edge_deletion_data,
+        EdgeUpdateEvent: get_edge_update_data,
+    }
+
+    event = event_cls(get_data_map[event_cls](event_conf))
+    event.validate()
+    return event
+
+
+def decode_event(routing_key, msg_body):
+    decode_map = {
+        'graph.node.create': NodeCreationEvent,
+        'graph.edge.create': EdgeCreationEvent,
+        'graph.node.delete': NodeDeletionEvent,
+        'graph.edge.delete': EdgeDeletionEvent,
+        'graph.edge.update': EdgeUpdateEvent,
+    }
+    decode_key = '.'.join(routing_key.split('.')[-3:])
+    event = decode_map[decode_key](json.loads(msg_body, object_hook=decode_dict))
+    event.validate()
+    return event
 
 
 class BasicEvent(object):
@@ -12,98 +89,95 @@ class BasicEvent(object):
     def msg(self):
         return json.dumps(self.data)
 
+    def validate(self):
+        raise NotImplemented()
+
 
 class NodeCreationEvent(BasicEvent):
 
-    def __init__(self, bl_obj=None):
-        super(NodeCreationEvent, self).__init__('graph.node.create')
-        if bl_obj:
-            data = {
-                'action': 'NODE_CREATE',
+    def  __init__(self, data):
+        super(NodeCreationEvent, self).__init__('graph.node.create', data)
+
+    def validate(self):
+        schema = Schema(
+            {
+                'action': 'NODE_CREATION',
                 'details': {
-                    'obj_class': type(bl_obj).__name__,
-                    'obj_id': bl_obj.id,
-                    'obj_hash': ome_hash(bl_obj.ome_obj)
+                    'obj_class': str,
+                    'obj_id': str,
+                    'obj_hash': int
                 }
             }
-            self.data = data
+        )
+        schema(self.data)
 
 
 class EdgeCreationEvent(BasicEvent):
 
-    def __init__(self, bl_act=None, bl_src_obj=None, bl_dest_obj=None):
-        super(EdgeCreationEvent, self).__init__('graph.edge.create')
-        if bl_act and bl_src_obj and bl_dest_obj:
-            data = {
+    def __init__(self, data):
+        super(EdgeCreationEvent, self).__init__('graph.edge.create', data)
+
+    def validate(self):
+        schema = Schema(
+            {
                 'action': 'EDGE_CREATE',
                 'details': {
-                    'act_type': type(bl_act).__name__,
-                    'act_id': bl_act.id,
-                    'act_hash': ome_hash(bl_act.ome_obj)
+                    'act_type': str,
+                    'act_id': str,
+                    'act_hash': int
                 },
-                'source_node': ome_hash(bl_src_obj.ome_obj),
-                'dest_node': ome_hash(bl_dest_obj.ome_obj)
+                'source_node': int,
+                'dest_node': int
             }
-            self.data = data
+        )
+        schema(self.data)
 
 
 class NodeDeletionEvent(BasicEvent):
 
-    def __init__(self, bl_obj=None):
-        super(NodeDeletionEvent, self).__init__('graph.node.delete')
-        if bl_obj:
-            data = {
+    def __init__(self, data):
+        super(NodeDeletionEvent, self).__init__('graph.node.delete', data)
+
+    def validate(self):
+        schema = Schema(
+            {
                 'action': 'NODE_DELETE',
-                'target': ome_hash(bl_obj.ome_obj)
+                'target': int
             }
-            self.data = data
+        )
+        schema(self.data)
 
 
 class EdgeDeletionEvent(BasicEvent):
 
-    def __init__(self, bl_act=None):
-        super(EdgeDeletionEvent, self).__init__('graph.edge.delete')
-        if bl_act:
-            data = {
+    def __init__(self, data):
+        super(EdgeDeletionEvent, self).__init__('graph.edge.delete', data)
+
+    def validate(self):
+        schema = Schema(
+            {
                 'action': 'EDGE_DELETE',
-                'target': ome_hash(bl_act.ome_obj)
+                'target': int
             }
-            self.data = data
+        )
+        schema(self.data)
 
 
 class EdgeUpdateEvent(BasicEvent):
 
-    def __init__(self, bl_act=None, bl_src_obj=None, bl_dest_obj=None):
-        super(EdgeUpdateEvent, self).__init__('graph.edge.update')
-        if bl_act:
-            data = {
+    def __init__(self, data):
+        super(EdgeUpdateEvent, self).__init__('graph.edge.update', data)
+
+    def validate(self):
+        def validate_optional_int(value):
+            if value is not None and type(value) != int:
+                raise ValueError('not integer or None')
+        schema = Schema(
+            {
                 'action': 'EDGE_UPDATE',
-                'target': ome_hash(bl_act.ome_obj),
-                'new_source_node': None,
-                'new_dest_node': None
+                'target': int,
+                'new_source_node': validate_optional_int,
+                'new_dest_node': validate_optional_int
             }
-            if bl_src_obj:
-                data['new_source_node'] = ome_hash(bl_src_obj.ome_obj)
-            if bl_dest_obj:
-                data['new_dest_node'] = ome_hash(bl_dest_obj.ome_obj)
-            self.data = data
-
-
-class EventsDecoder(object):
-
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def decode(routing_key, msg_body):
-        decode_map = {
-            'graph.node.create': NodeCreationEvent,
-            'graph.edge.create': EdgeCreationEvent,
-            'graph.node.delete': NodeDeletionEvent,
-            'graph.edge.delete': EdgeDeletionEvent,
-            'graph.edge.update': EdgeUpdateEvent
-        }
-        decode_key = '.'.join(routing_key.split('.')[-3:])
-        event = decode_map[decode_key]()
-        event.data = json.loads(msg_body)
-        return event
+        )
+        schema(self.data)
