0a059b882da63ebde247945922156ff7e99d4976
simleo <simleo@crs4.it>
2013-04-15 18:02:01 +0200

    updated convert_dbsnp

--- bl/vl/app/snp_manager/common.py
+++ bl/vl/app/snp_manager/common.py
@@ -119,12 +119,17 @@ def build_index_key(seq):
   return min(seq, rc(seq))
 
 
-def write_mdef(stream, fo):
+def write_mdef(stream, fo, header=True):
   """
   Given a stream of [label, mask, index, allele_flip] rows, write a
   tsv file suitable for input to the marker set importer.
+
+  If header is False, field names will not be written in the first row
+  (this is useful if you want to generate the output file with
+  multiple calls to this function).
   """
   writer = csv.writer(fo, delimiter="\t", lineterminator=os.linesep)
-  writer.writerow(MARKER_DEF_FIELDS)
+  if header:
+    writer.writerow(MARKER_DEF_FIELDS)
   for row in stream:
     writer.writerow(map(str, row))
