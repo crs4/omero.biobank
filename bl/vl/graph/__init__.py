331582d228c4449d3078413d5ce50c3ec310197b
Luca Lianas <lianas@crs4.it>
2013-05-30 09:42:58 +0200

    fixed library imports for neo4j drivers factory

--- bl/vl/graph/__init__.py
+++ bl/vl/graph/__init__.py
@@ -1,16 +1,16 @@
-from drivers.neo4j import Neo4JDriver
-from drivers.local_pygraph import PygraphDriver
 import bl.vl.utils.graph as grconf
 
 
 def build_driver(kb=None):
 
     def build_neo4j_driver(kb):
+        from drivers.neo4j import Neo4JDriver
         d = Neo4JDriver(grconf.graph_uri(), grconf.graph_username(),
                         grconf.graph_password(), kb)
         return d
 
     def build_local_memory_driver(kb):
+        from drivers.local_pygraph import PygraphDriver
         d = PygraphDriver(kb, kb.logger)
         return d
 
