5c7a47df177a18407e58aa5bb00e90a9b998ecc2
Luca Lianas <lianas@crs4.it>
2013-07-09 17:33:08 +0200

    new utility used to build a unique identifier for graph edges

--- bl/vl/utils/graph.py
+++ bl/vl/utils/graph.py
@@ -44,3 +44,7 @@ def graph_password():
             return getattr(blconf, var)
         except AttributeError:
             raise ValueError("Cant't find config valuer for %s" % var)
+
+
+def build_edge_id(source_node_hash, dest_node_hash):
+    return '%s::%s' % (source_node_hash, dest_node_hash)
