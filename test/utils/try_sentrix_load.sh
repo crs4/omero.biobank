85b7cba801001768d520df4d74e527b594cb64dd
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2013-07-25 10:50:27 +0200

    added a minimal try script

---
+++ test/utils/try_sentrix_load.sh
@@ -0,0 +1,22 @@
+SENTRIX_LOADER=../../utils/sentrix_load_mapping
+
+ls sentrix_load_data/CSV/*.csv > csv.list
+ls sentrix_load_data/SDF/*.sdf > sdfs.list
+
+${SENTRIX_LOADER} --csvs csv.list
+${SENTRIX_LOADER} --sdfs sdfs.list
+${SENTRIX_LOADER} --check-consistency
+
+rm -f illumina_chips.db
+rm -f sentrix_load_mapping.log
+rm -f csv.list sdfs.list
+
+
+
+
+
+
+
+
+
+
