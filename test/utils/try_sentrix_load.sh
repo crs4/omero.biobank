a806257b9295053035bb05be3bb2fbbe0ffa4ce0
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2013-08-28 14:53:00 +0200

    now we normalize the plate_well labels

--- test/utils/try_sentrix_load.sh
+++ test/utils/try_sentrix_load.sh
@@ -1,5 +1,9 @@
 SENTRIX_LOADER=../../utils/sentrix_load_mapping
 
+rm -f illumina_chips.db
+rm -f sentrix_load_mapping.log
+rm -f csv.list sdfs.list
+
 ls sentrix_load_data/CSV/*.csv > csv.list
 ls sentrix_load_data/SDF/*.sdf > sdfs.list
 
@@ -7,9 +11,6 @@ ${SENTRIX_LOADER} --csvs csv.list
 ${SENTRIX_LOADER} --sdfs sdfs.list
 ${SENTRIX_LOADER} --check-consistency
 
-rm -f illumina_chips.db
-rm -f sentrix_load_mapping.log
-rm -f csv.list sdfs.list
 
 
 
