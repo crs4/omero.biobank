55dd84436ff7990182e9eb0fcf1904497e9ac85b
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2013-08-29 10:07:16 +0200

    try_load_from_yaml.sh: added basic handling of command line arguments

--- test/serialize/try_load_from_yaml.sh
+++ test/serialize/try_load_from_yaml.sh
@@ -1,7 +1,10 @@
 export OMERO_BIOBANK_EXTRA_MODULES='illumina_chips'
 
-DBNAME=test_illumina_chips.db
-YMLFNAME=illumina_load.yml
+DBNAME_DEFAULT=test_illumina_chips.db
+YMLFNAME_DEFAULT=illumina_load.yml
+
+DBNAME=${1:-${DBNAME_DEFAULT}}
+YMLFNAME=${2:-${YMLFNAME_DEFAULT}}
 
 python convert_illumina.py ${DBNAME} ${YMLFNAME} 
 python try_load.py ${YMLFNAME}
