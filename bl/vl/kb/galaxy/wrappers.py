c701c87ebde154465d8ba0ee78ca07b6d6cebaa3
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2013-09-06 13:49:15 +0200

    wrappers.py: fixed Workflow.clone()

--- bl/vl/kb/galaxy/wrappers.py
+++ bl/vl/kb/galaxy/wrappers.py
@@ -1,4 +1,5 @@
 from bl.vl.kb.galaxy.core_wrappers import Workflow as CoreWorkflow
+from bl.vl.kb.galaxy.core_wrappers import History as CoreHistory
 
 class Workflow(CoreWorkflow):
     def __init__(self, wf_id, wf_dict):
@@ -6,11 +7,21 @@ class Workflow(CoreWorkflow):
         setattr(self.core, 'id', wf_id)
 
     def clone(self):
-        return self.__class__(None, self.to_json())
+        return self.__class__(None, self.core.wrapped)
         
     @property
     def id(self):
         return self.core.id
+
+    def __eq__(self, other):
+        return  (self.id == other.id 
+                 and super(Workflow, self).__eq__(other))
+     
+
+class History(CoreHistory):
+    pass
+
+
 
     
         
