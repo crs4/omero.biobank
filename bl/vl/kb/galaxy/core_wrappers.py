5fc15d5845b9f9bf43fc6d18b4fc23745c202e37
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2013-09-06 09:55:10 +0200

    core_wrappers:Workflow passes all tests in test_core_wrappers.py

--- bl/vl/kb/galaxy/core_wrappers.py
+++ bl/vl/kb/galaxy/core_wrappers.py
@@ -8,13 +8,16 @@ import json
 
 class Wrapper(object):
     # http://stackoverflow.com/questions/2827623/python-create-object-and-add-attributes-to-it
-    def __init__(self, wrapped):
+    def __init__(self, wrapped, parent=None):
         object.__setattr__(self, 'core', lambda: None)
         object.__setattr__(self, 'is_modified', False)        
+        object.__setattr__(self, 'parent', parent)    
         setattr(self.core, 'wrapped', wrapped)
 
     def touch(self):
-        object.__setattr__(self, 'is_modified', True)                
+        object.__setattr__(self, 'is_modified', True)
+        if self.parent:
+            self.parent.touch()               
 
     def __setattr__(self, name, value):
         core = self.core
@@ -29,11 +32,8 @@ class Wrapper(object):
             return core.wrapped[name]
         else:
             raise KeyError(name)
-    def sync(self):
-        pass
 
     def to_json(self):
-        self.sync()
         #return json.dumps(self.core.wrapped)
         return self.core.wrapped
 
@@ -43,17 +43,19 @@ class Wrapper(object):
         
 
 class Tool(object):
-    def __init__(self, step_dict):
+    def __init__(self, step_dict, parent):
         self.step_dict = step_dict
         self.state = json.loads(step_dict['tool_state'])
-        
+        self.parent = parent
     @property
     def id(self):
         return self.step_dict['tool_id']
     @property
     def version(self):
         return self.step_dict['tool_version']
-
+    @property
+    def errors(self):
+        return self.step_dict['tool_errors']
     @property
     def params(self):
         return self.state
@@ -63,23 +65,22 @@ class Tool(object):
         if not self.state.has_key(key):
             raise ValueError(key)
         self.state[key] = json.dumps(value)
+        self.parent.touch()
+        self.sync()
         
     def sync(self):
         self.step_dict['tool_state'] = json.dumps(self.state)
         
 class Step(Wrapper):
-    def __init__(self, step_dict):
-        super(Step, self).__init__(step_dict)
+    def __init__(self, step_dict, parent):
+        super(Step, self).__init__(step_dict, parent)
         if step_dict['type'] == 'tool':
-            setattr(self.core, 'tool', Tool(step_dict))
+            setattr(self.core, 'tool', Tool(step_dict, self))
     
     @property
     def tool(self):
         return self.core.tool
         
-    def sync(self):
-        if self.type == 'tool':
-            self.tool.sync()
         
 class Workflow(Wrapper):
     """
@@ -90,15 +91,11 @@ class Workflow(Wrapper):
         super(Workflow, self).__init__(wf_dict)
         steps = wf_dict['steps']
         setattr(self.core, 'steps', 
-                [Step(steps[str(x)]) for x in xrange(len(steps))])
+                [Step(steps[str(x)], self) for x in xrange(len(steps))])
     @property
     def steps(self):
         return self.core.steps
         
-    def sync(self):
-        for s in self.steps:
-            s.sync()
-
     def __eq__(self, other):
         return  self.name == other.name
 
--- test/galaxy/test_core_wrappers.py
+++ test/galaxy/test_core_wrappers.py
@@ -31,12 +31,49 @@ class TestCoreWrappers(unittest.TestCase):
       if k in ['steps']:
         continue
       self.assertEqual(getattr(wf, k), wk_desc[k])
-        
+    self.assertFalse(wf.is_modified)
+    wf.annotation = 'a new annotation'
+    self.assertTrue(wf.is_modified)
+
+  def test_workflow_steps(self):
+    with open('Galaxy-Workflow-Hepatocyte.ga') as f:
+      wk_desc = json.load(f)
+    wf = cwps.Workflow(wk_desc)
+    for s in wf.steps:
+      s_desc = wk_desc['steps'][str(s.id)]
+      for k in s_desc:
+        if k in ['tool_errors', 'tool_id', 'tool_version']:
+          if s.type == 'tool':
+            self.assertEqual(getattr(s.tool, k.replace('tool_', '')), s_desc[k])
+          continue
+        self.assertEqual(getattr(s, k), s_desc[k])
+    self.assertFalse(wf.is_modified)
+
+  def test_workflow_step_taint(self):
+    with open('Galaxy-Workflow-Hepatocyte.ga') as f: 
+      wk_desc = json.load(f)
+    wf = cwps.Workflow(wk_desc)
+    self.assertFalse(wf.is_modified)    
+    wf.steps[0].annotation = 'a new annotation'
+    self.assertTrue(wf.is_modified)        
+
+  def test_workflow_tool_taint(self):
+    with open('Galaxy-Workflow-Hepatocyte.ga') as f: 
+      wk_desc = json.load(f)
+    wf = cwps.Workflow(wk_desc)
+    self.assertFalse(wf.is_modified)    
+    wf.steps[4].tool['format'] = 'a new format'
+    self.assertTrue(wf.is_modified)        
+
+    
 def suite():
   suite = unittest.TestSuite()
   suite.addTest(TestCoreWrappers('test_wrapper_initialize'))
   suite.addTest(TestCoreWrappers('test_wrapper_taint'))  
   suite.addTest(TestCoreWrappers('test_workflow_initialize'))
+  suite.addTest(TestCoreWrappers('test_workflow_steps'))  
+  suite.addTest(TestCoreWrappers('test_workflow_step_taint'))    
+  suite.addTest(TestCoreWrappers('test_workflow_tool_taint'))      
   return suite
 
 if __name__ == '__main__':
