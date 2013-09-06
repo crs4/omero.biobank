a0fe821b6ee307a1a16eedd5cdfb0b5d59bfbfba
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2013-09-06 22:45:01 +0200

    wrappers.py: added Workflow.inputs

--- bl/vl/kb/galaxy/wrappers.py
+++ bl/vl/kb/galaxy/wrappers.py
@@ -2,13 +2,16 @@ from bl.vl.kb.galaxy.core_wrappers import Workflow as CoreWorkflow
 from bl.vl.kb.galaxy.core_wrappers import History as CoreHistory
 
 class Workflow(CoreWorkflow):
-    def __init__(self, wf_id, wf_dict):
+    def __init__(self, wf_id, wf_dict, wf_inputs=None):
         super(Workflow, self).__init__(wf_dict)
         setattr(self.core, 'id', wf_id)
+        setattr(self.core, 'inputs', wf_inputs)        
     
     def touch(self):
         super(Workflow, self).touch()
-        setattr(self.core, 'id', None)        
+        # forget all galaxy connections
+        setattr(self.core, 'id', None)
+        setattr(self.core, 'inputs', None)    
 
     def clone(self):
         return self.__class__(None, self.core.wrapped)
@@ -16,6 +19,9 @@ class Workflow(CoreWorkflow):
     @property
     def id(self):
         return self.core.id
+    @property
+    def inputs(self):
+        return self.core.inputs
 
     def __eq__(self, other):
         return  (self.id == other.id 
