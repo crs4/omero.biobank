1aa5d88fe64761c11a53df7e6e8f229673fb20d8
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2013-09-06 22:45:01 +0200

    wrappers.py: added Workflow.inputs

--- test/galaxy/test_wrappers.py
+++ test/galaxy/test_wrappers.py
@@ -23,13 +23,38 @@ class TestWrappers(unittest.TestCase):
     wf = glxy.Workflow(wf_id, wf_desc)
     self.assertEqual(wf.id, wf_id)
     wf2 = wf.clone()
+    self.assertFalse(wf2 == wf)        
     self.assertEqual(wf2.id, None)    
     self.assertEqual(wf.to_json(), wf2.to_json())
+
+  def test_workflow_inputs(self):
+    with open('Galaxy-Workflow-Hepatocyte.ga') as f: 
+      wf_desc = json.load(f)
+    wf_id = '89898989898'
+    wf_inputs = {u'contigs': {'input': u'63',
+                              'mimetype': u'x-vl/fasta',
+                              'type': u'SeqDataSample'
+                              },
+                 u'mates': {'input': u'65',
+                            'mimetype': u'x-vl/fastq',
+                            'type': u'SeqDataSample'
+                            },
+                 u'reads': {'input': u'64',
+                            'mimetype': u'x-vl/fastq',
+                            'type': u'SeqDataSample'
+                            }
+                }
+    wf = glxy.Workflow(wf_id, wf_desc, wf_inputs)
+    self.assertEqual(set(wf.inputs.keys()), set(wf_inputs.keys()))
+    for k in wf.inputs:
+      self.assertEqual(wf.inputs[k], wf_inputs[k])
+
     
 def suite():
   suite = unittest.TestSuite()
   suite.addTest(TestWrappers('test_workflow_init'))
   suite.addTest(TestWrappers('test_workflow_clone'))  
+  suite.addTest(TestWrappers('test_workflow_inputs'))    
   return suite
 
 if __name__ == '__main__':
