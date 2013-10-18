e9e78dc2d7b3c116af409128107f239b8e11b359
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2013-10-18 22:06:14 +0200

    added python wrappers for he new models

--- bl/vl/kb/drivers/omero/sequencing.py
+++ bl/vl/kb/drivers/omero/sequencing.py
@@ -1,8 +1,7 @@
-import omero.model as om
-import omero.rtypes as ort
-
 import wrapper as wp
 from data_samples import DataSample
+from utils import make_unique_key
+
 from vessels import Tube
 from objects_collections import Lane
 
@@ -20,3 +19,38 @@ class RawSeqDataSample(DataSample):
 class SeqDataSample(DataSample):  
   OME_TABLE = 'SeqDataSample'
   __fields__ = [('sample', Tube, wp.OPTIONAL)]
+
+class ReferenceGenome(DataSample):
+  OME_TABLE = 'ReferenceGenome'
+  __fields__ = [('nChroms', wp.INT, wp.REQUIRED),
+                ('maker', wp.STRING, wp.REQUIRED),
+                ('model', wp.STRING, wp.REQUIRED),
+                ('release', wp.STRING, wp.REQUIRED),
+                ]
+  def __preprocess_conf__(self, conf):
+    if not 'referenceGenomeUK' in conf:
+      conf['referenceGenomeUK'] = make_unique_key(conf['maker'], conf['model'],
+                                                  conf['release'])
+    return super(ReferenceGenome, self).__preprocess_conf__(conf)
+
+class AlignedSeqDataSample(DataSample):  
+  OME_TABLE = 'AlignedSeqDataSample'
+  __fields__ = [('referenceGenome', ReferenceGenome, wp.REQUIRED)]
+
+class GenomicAssemblyData(DataSample):  
+  OME_TABLE = 'GenomicAssemblyData'
+  __fields__ = []
+  
+class GenomeVariationsData(DataSample):  
+  OME_TABLE = 'GenomeVariationsData'
+  __fields__ = [('referenceGenome', ReferenceGenome, wp.REQUIRED)]  
+
+class GeneExpressionLevels(DataSample):  
+  OME_TABLE = 'GeneExpressionLevels'
+  __fields__ = []
+
+class TaxonomicProfile(DataSample):  
+  OME_TABLE = 'TaxonomicProfile'
+  __fields__ = []
+  
+  
