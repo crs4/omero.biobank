b90e0e16040ab33feb4c6e6565e212730e7eb199
Luca Lianas <lianas@crs4.it>
2014-02-26 15:43:54 +0100

    added permissions support

--- bl/vl/kb/drivers/omero/admin.py
+++ bl/vl/kb/drivers/omero/admin.py
@@ -3,7 +3,14 @@
 
 from bl.vl.kb import KBPermissionError
 
+import omero
+from omero_model_PermissionsI import PermissionsI as Perm
+
 class Admin(object):
+
+  GROUP_PERM_PRIVATE = 'rw----'
+  GROUP_PERM_READ_ONLY = 'rwra--'
+  GROUP_PERM_READ_WRITE = 'rwrw--'
 
   def __init__(self, kb):
     self.kb = kb
@@ -38,3 +45,19 @@ class Admin(object):
     else:
       raise KBPermissionError('User %s is not leader of group %s, can\'t move objects to common space' %
                               (self.kb.user, self.kb.get_current_group()[0]))
+
+  def change_group_permissions(self, group, perms):
+    if perms not in (self.GROUP_PERM_PRIVATE, self.GROUP_PERM_READ_ONLY,
+                     self.GROUP_PERM_READ_WRITE):
+      raise ValueError('Permission string "%s" is not a valid one' % perms)
+    else:
+      if not self.kb.current_session:
+        self.kb.connect()
+      try:
+          p = Perm(perms)
+          g = self.kb._get_group(group)
+          a = self.kb.current_session.getAdminService()
+          a.changePermissions(g, p)
+      except omero.SecurityViolation:
+        raise KBPermissionError('User %s is not allowed to change group permissions for group %s' %
+                                (self.kb.user, self.kb.get_current_group[0]))
