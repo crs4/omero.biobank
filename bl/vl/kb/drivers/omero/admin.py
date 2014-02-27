a20a3c95c0f753729e0d1877f7ec7d7c63f9705b
Luca Lianas <lianas@crs4.it>
2014-02-27 10:28:33 +0100

    added doc

--- bl/vl/kb/drivers/omero/admin.py
+++ bl/vl/kb/drivers/omero/admin.py
@@ -8,6 +8,18 @@ from omero_model_PermissionsI import PermissionsI as Perm
 
 class Admin(object):
 
+  """
+  The three permission levels work as follow:
+  * GROUP_PERM_PRIVATE: each user of the group can read\write only the data produced by
+                        himself
+  * GROUP_PERM_READ_ONLY: users can read data produced by other users in the same group
+                          but only the user that created the data can update or delete them,
+                          users can also read data from OMERO.tables created by other users
+                          in the same group but can't write new rows in the tables
+  * GROUP_PERM_READ_WRITE: users can read\write\delete data created by other users in the
+                           same group and can also write data in OMERO.tables created by one
+                           user of the same group
+  """
   GROUP_PERM_PRIVATE = 'rw----'
   GROUP_PERM_READ_ONLY = 'rwra--'
   GROUP_PERM_READ_WRITE = 'rwrw--'
@@ -46,7 +58,17 @@ class Admin(object):
       raise KBPermissionError('User %s is not leader of group %s, can\'t move objects to common space' %
                               (self.kb.user, self.kb.get_current_group()[0]))
 
-  def change_group_permissions(self, group, perms):
+  def change_group_permissions(self, group_name, perms):
+    """
+    only group owners and admins can change group permissions
+    admin can change permissions for all the groups
+    owners can change permissions only for the group they are leaders of
+
+    .. code-block:: python
+
+       kb = KB(driver='omero')('localhost', 'root', 'ROOT_PASSWD')
+       kb.admin.change_group_permissions('foogroup', kb.admin.GROUP_PERM_xxx)
+    """
     if perms not in (self.GROUP_PERM_PRIVATE, self.GROUP_PERM_READ_ONLY,
                      self.GROUP_PERM_READ_WRITE):
       raise ValueError('Permission string "%s" is not a valid one' % perms)
@@ -55,9 +77,9 @@ class Admin(object):
         self.kb.connect()
       try:
           p = Perm(perms)
-          g = self.kb._get_group(group)
+          g = self.kb._get_group(group_name)
           a = self.kb.current_session.getAdminService()
           a.changePermissions(g, p)
       except omero.SecurityViolation:
         raise KBPermissionError('User %s is not allowed to change group permissions for group %s' %
-                                (self.kb.user, self.kb.get_current_group[0]))
+                                (self.kb.user, group_name))
