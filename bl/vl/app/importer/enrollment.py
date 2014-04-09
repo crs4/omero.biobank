3ac1adb4f442ca4af11f90bec0ad5b13d9fd4c9c
Luca Lianas <lianas@crs4.it>
2014-04-09 15:28:35 +0200

    enrollments importer handles multiple studies

--- bl/vl/app/importer/enrollment.py
+++ bl/vl/app/importer/enrollment.py
@@ -192,14 +192,17 @@ def implementation(logger, host, user, passwd, args, close_handles):
                           delimiter='\t', lineterminator=os.linesep,
                           extrasaction='ignore')
   report.writeheader()
-  try:
-    recorder.record(records, o, report,
-                    args.blocking_validator)
-  except core.ImporterValidationError as ve:
-    logger.critical(ve.message)
-    raise
-  finally:
-    close_handles(args)
+  records_map = Recorder.map_by_column(records, 'study')
+  for study_label, records in records_map.iteritems():
+      try:
+        logger.info('Dumping %d records with study %s as reference', len(records), study_label)
+        recorder.record(records, o, report,
+                        args.blocking_validator)
+      except core.ImporterValidationError as ve:
+        logger.critical(ve.message)
+        close_handles(args)
+        raise ve
+  close_handles(args)
   logger.info('done processing file %s' % args.ifile.name)
 
 
