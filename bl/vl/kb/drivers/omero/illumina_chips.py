66ac768ae3295131bff9b3adcabec4e8fa44a058
Luca Lianas <lianas@crs4.it>
2014-12-01 15:17:30 +0100

    new enums
    Signed-off-by: Luca Lianas <lianas@crs4.it>

--- bl/vl/kb/drivers/omero/illumina_chips.py
+++ bl/vl/kb/drivers/omero/illumina_chips.py
@@ -36,7 +36,7 @@ class IlluminaBeadChipAssayType(wp.OmeroWrapper):
 
 class IlluminaArrayOfArraysType(wp.OmeroWrapper):
   OME_TABLE = "IlluminaArrayOfArraysType"
-  __enums__ = ["BeadChip_12x1Q", "UNKNOWN", "BeadChip_12x8"]
+  __enums__ = ["BeadChip_12x1Q", "UNKNOWN", "BeadChip_12x8", "BeadChip_24x1x4"]
 
 
 class IlluminaArrayOfArraysClass(wp.OmeroWrapper):
@@ -46,7 +46,7 @@ class IlluminaArrayOfArraysClass(wp.OmeroWrapper):
 
 class IlluminaArrayOfArraysAssayType(wp.OmeroWrapper):
   OME_TABLE = "IlluminaArrayOfArraysAssayType"
-  __enums__ = ["Infinium_HD", "UNKNOWN", "Infinium_NXT"]
+  __enums__ = ["Infinium_HD", "UNKNOWN", "Infinium_NXT", "Infinium_LCG"]
 
 
 class IlluminaArrayOfArrays(TiterPlate):
